3b1f310bc41137c817ff3e34d61c2b2fec745944
Nathan Struhs <nathan.struhs@getg5.com>
2018-10-30 19:51:52 -0700

    Render component helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def render_component(component_name, locals = {}, &block)
+    name = component_name.split("_").first
+    render("components/#{name}/#{component_name}", locals, &block)
+  end
 end

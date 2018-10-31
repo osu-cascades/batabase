843d847ac0183c9d65330b9939ba60cb6626d542
Nathan Struhs <nathan.struhs@getg5.com>
2018-10-30 20:09:46 -0700

    Component generator

---
+++ lib/generators/component_generator.rb
@@ -0,0 +1,33 @@
+class ComponentGenerator < Rails::Generators::Base
+  argument :component_name, required: true, desc: "Component name, e.g: button"
+
+  def create_view_file
+    create_file "#{component_path}/_#{component_name}.html.erb"
+  end
+
+  def create_css_file
+    create_file "#{component_path}/#{component_name}.scss"
+  end
+
+  def create_js_file
+    create_file "#{component_path}/#{component_name}.js" do
+      "import \"./#{component_name}.scss\";\n"
+    end
+  end
+
+  def require_component
+    File.open(init_path, 'a') do |file|
+      file.puts "import '../components/#{component_name}/#{component_name}'"
+    end
+  end
+
+  protected
+
+  def component_path
+    "app/javascript/components/#{component_name}"
+  end
+
+  def init_path
+    'app/javascript/init/index.js'
+  end
+end

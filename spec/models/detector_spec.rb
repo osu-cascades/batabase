a1584a80841524e1b20d2ee1e8a65ba160348876
Nathan Struhs <nathan.struhs@getg5.com>
2018-10-31 10:18:11 -0700

    Spec: refactor detector validations

--- spec/models/detector_spec.rb
+++ spec/models/detector_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe Detector, type: :model do
   subject(:detector) { build(:detector) }
 
   describe 'validations' do
-    it { should validate_presence_of(:code) }
-    it { should validate_presence_of(:serial_num) }
-    it { should validate_presence_of(:manufacturer) }
+    %i(code serial_num manufacturer).each do |attribute|
+      it { should validate_presence_of(attribute) }
+    end
 
     it { should validate_length_of(:code).is_at_most(20) }
-    it { should validate_length_of(:serial_num).is_at_most(50) }
-    it { should validate_length_of(:model).is_at_most(50) }
-    it { should validate_length_of(:manufacturer).is_at_most(50) }
-    it { should validate_length_of(:firmware).is_at_most(50) }
+
+    %i(serial_num model manufacturer firmware).each do |attribute|
+      it { should validate_length_of(attribute).is_at_most(50) }
+    end
   end
 end

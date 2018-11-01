32570758cae0a9c06af227e05fbe85593deab718
Nathan Struhs <nathan.struhs@getg5.com>
2018-10-31 19:21:23 -0700

    Minor formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, presence: true
+  validates_length_of :middle_init, maximum: 4
+  validates_length_of :initials, maximum: 5
+  validates_length_of :state_code, maximum: 8
+  validates_length_of :work_phone, maximum: 15
   validates_length_of :last_name,
                       :first_name,
                       :organization,
@@ -11,11 +15,5 @@ class User < ApplicationRecord
                       :zip_code,
                       :country,
                       :email_address,
-                      :work_extension,
-                      maximum: 50
-  validates_length_of :middle_init, maximum: 4
-  validates_length_of :initials, maximum: 5
-  validates_length_of :state_code, maximum: 8
-  validates_length_of :work_phone, maximum: 15
-
+                      :work_extension, maximum: 50
 end

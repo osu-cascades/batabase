9ef7381251c6dfaa4e85c8d0db6b5d1afef32bb2
Nathan Struhs <nathan.struhs@getg5.com>
2018-11-01 18:40:44 -0700

    Update models and specs to reflect column drops

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,8 @@
 class User < ApplicationRecord
-  validates :first_name, :last_name, presence: true
-  validates_length_of :middle_init, maximum: 4
-  validates_length_of :initials, maximum: 5
+  validates :first_name, :last_name, :email, presence: true
   validates_length_of :state_code, maximum: 8
-  validates_length_of :work_phone, maximum: 15
   validates_length_of :last_name,
                       :first_name,
                       :organization,
-                      :position_title,
-                      :address_type,
-                      :address,
-                      :address_2,
-                      :city,
-                      :zip_code,
-                      :country,
-                      :email_address,
-                      :work_extension, maximum: 50
+                      :email, maximum: 50
 end
--- spec/models/detector_spec.rb
+++ spec/models/detector_spec.rb
@@ -4,11 +4,9 @@ RSpec.describe Detector, type: :model do
   subject(:detector) { build(:detector) }
 
   describe 'validations' do
-    %i(code serial_num manufacturer).each do |attribute|
+    %i(serial_num manufacturer).each do |attribute|
       it { should validate_presence_of(attribute) }
     end
-
-    it { should validate_length_of(:code).is_at_most(20) }
 
     %i(serial_num model manufacturer firmware).each do |attribute|
       it { should validate_length_of(attribute).is_at_most(50) }

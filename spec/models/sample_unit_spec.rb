af4f1aa90761e68cbd5d2f58d04a418f8d87e946
Nathan Struhs <nathan.struhs@getg5.com>
2018-11-07 08:36:25 -0800

    Add sample_unit & software models

---
+++ spec/models/sample_unit_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe SampleUnit, type: :model do
+  subject(:sample_unit) { build(:sample_unit) }
+
+  describe 'validations' do
+    it { should validate_presence_of(:site_code) }
+    it { should validate_length_of(:site_code).is_at_most(20) }
+    it { should validate_length_of(:federal_agency).is_at_most(50) }
+  end
+end

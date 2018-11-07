3c9a975de90eb619cb6437031fe20d036e180c5d
Nathan Struhs <nathan.struhs@getg5.com>
2018-11-07 08:38:31 -0800

    Clean up formatting

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Report, type: :model do
     end
 
     %i(report_label report_name).each do |attribute|
-      it {should validate_length_of(attribute).is_at_most(255)}
+      it { should validate_length_of(attribute).is_at_most(255) }
     end
   end
 end

5b09fc471898a46177c4a7823c00ac561af9eb22
Nathan Struhs <nathan.struhs@getg5.com>
2018-11-11 11:26:27 -0800

    Letter opener configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,8 @@ Rails.application.configure do
   config.active_record.verbose_query_logs = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 
 
   # Raises error for missing translations

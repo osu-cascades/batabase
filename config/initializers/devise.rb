6e1db185b5fe2ab2ba3df27e87110ccd1ab28cc9
Nathan Struhs <nathan.struhs@getg5.com>
2018-11-11 11:17:44 -0800

    Remove reconfirmable config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = 'd4547457a07c6412b72c1f4f0601a5871c38aa8d3e71f699e0ce4b77720112eeb434b556a54c770f8713f8ba920705db91b0afdaf03bff47924cf19e2ff810ce'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -142,7 +142,7 @@ Devise.setup do |config|
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
   # db field (see migrations). Until confirmed, new email is stored in
   # unconfirmed_email column, and copied to email column on successful confirmation.
-  config.reconfirmable = true
+  config.reconfirmable = false
 
   # Defines which key will be used when confirming an account
   # config.confirmation_keys = [:email]

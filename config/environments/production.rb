5e6241dfcbb1d96c8509b2bfd08f3a90c63215c8
Nathan Struhs <nathan.struhs@getg5.com>
2018-11-12 10:26:57 -0800

    Moare email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "batabase_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => 'batabase-staging.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'batabase-staging.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

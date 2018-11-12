74b02944f1ad7b26361269512f6c1c2ef42d05b9
Nathan Struhs <nathan.struhs@getg5.com>
2018-11-12 11:10:50 -0800

    Clean up navbar Add some js to update hamburger on click Add all top level navbar items Replace sign out button w/ user email & dropdown

--- app/javascript/components/nav-bar/nav-bar.js
+++ app/javascript/components/nav-bar/nav-bar.js
@@ -1,1 +1,28 @@
 import './nav-bar.scss';
+
+class HamburgerMenu {
+  constructor() {
+    this.burgers = this.getBurgers();
+    if (this.burgers.length > 0) {
+      this.burgerListener();
+    }
+  }
+
+  getBurgers() {
+    return Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  }
+
+  burgerListener(burgers) {
+    this.burgers.forEach(patty => {
+      patty.addEventListener('click', () => {
+        const target = document.getElementById(patty.dataset.target);
+        patty.classList.toggle('is-active');
+        target.classList.toggle('is-active');
+      });
+    });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburgerMenu = new HamburgerMenu();
+});

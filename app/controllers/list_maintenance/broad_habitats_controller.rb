f05e3437b66b6925757177037843f2a592c52b7c
Nathan Struhs <nathan.struhs@getg5.com>
2018-11-15 14:59:16 -0800

    Broad Habitat: new, edit

--- app/controllers/list_maintenance/broad_habitats_controller.rb
+++ app/controllers/list_maintenance/broad_habitats_controller.rb
@@ -1,5 +1,49 @@
 class ListMaintenance::BroadHabitatsController < ApplicationController
+  before_action :set_broad_habitat, only: [:edit, :update, :destroy]
+
   def index
     @broad_habitats = BroadHabitat.all
   end
+
+  def new
+    @broad_habitat = BroadHabitat.new
+  end
+
+  def edit; end
+
+  def create
+    @broad_habitat = BroadHabitat.new(broad_habitat_params)
+    if @broad_habitat.save
+      redirect_to broad_habitats_path
+      flash[:success] = 'Broad habitat was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @broad_habitat.update(broad_habitat_params)
+      redirect_to broad_habitats_path
+      flash[:success] = 'Broad habitat was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @broad_habitat.destroy
+    redirect_to broad_habitats_path
+    flash[:success] = 'Broad habitat was successfully destroyed.'
+  end
+
+  private
+
+  def broad_habitat_params
+    params.require(:broad_habitat).permit(:id, :label)
+  end
+
+  def set_broad_habitat
+    @broad_habitat = BroadHabitat.find(params[:id])
+  end
+
 end

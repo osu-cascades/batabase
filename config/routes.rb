19a37d819c06e79bb954af827a28cdc796692787
Nathan Struhs <nathan.struhs@getg5.com>
2018-11-15 13:40:05 -0800

    Remove list maintenance controller/view

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  get 'list_maintenance', to: 'list_maintenance#index'
-
   namespace :list_maintenance do
     resources :broad_habitats, only: [:index]
   end

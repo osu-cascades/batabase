5069cdb6c9fcb9d4e7d60e45e38b3f6a6e2bed57
scmilee <drudged@oregonstate.edu>
2018-11-18 09:41:40 -0800

    update routes for the new controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ Rails.application.routes.draw do
   root to: 'static#home'
 
   devise_for :users
-
+  resources :deployments, except: [:show]
+  resources :sample_units, except: [:show]
   namespace :list_maintenance, as: '' do
     resources :broad_habitats, except: [:show]
     resources :contacts, except: [:show]
+    resources :clutter_types, except: [:show]
+    resources :distance_ranges, except: [:show]
+    resources :local_habitats, except: [:show]
     resources :organizations, except: [:show]
+    resources :speciess, except: [:show]
+    resources :species_groups, except: [:show]
   end
 end

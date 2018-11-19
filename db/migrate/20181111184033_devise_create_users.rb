f7b0f4ef06e8278f56b9b3113d5f625e5f569ce6
Nathan Struhs <nathan.struhs@getg5.com>
2018-11-19 07:31:37 -0800

    Update users migration w/ new index

--- db/migrate/20181111184033_devise_create_users.rb
+++ db/migrate/20181111184033_devise_create_users.rb
@@ -14,19 +14,18 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       ## Rememberable
       t.datetime :remember_created_at
 
-  
+
       ## Confirmable
       t.string   :confirmation_token
       t.datetime :confirmed_at
       t.datetime :confirmation_sent_at
 
-      
+
       t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    add_index :users, :confirmation_token,   unique: true
   end
 end

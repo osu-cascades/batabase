4ec35864b23171619db34e733ecd671b5f7536ad
Nathan Struhs <nathan.struhs@getg5.com>
2018-11-20 15:34:34 -0800

    Startup script and seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  :email => 'nathanstruhs@gmail.com',
+  :password => 'password',
+  :password_confirmation => 'password',
+  confirmed_at: Time.now.utc
+)
+
+User.create!(
+  :email => 'drudged@oregonstate.edu',
+  :password => 'password',
+  :password_confirmation => 'password',
+  confirmed_at: Time.now.utc
+)

6d74e01f34117002567392c3d0289266d6888377
Nathan Struhs <nathan.struhs@getg5.com>
2018-11-20 15:34:34 -0800

    Startup script and seed db

---
+++ createdb.sh
@@ -0,0 +1,7 @@
+cd lib/;
+psql -d postgres -c "DROP DATABASE batabase_development";
+createdb batabase_development;
+psql -d batabase_development -f dump.sql;
+psql -d batabase_development -f update-db.sql;
+cd ..;
+rails db:setup;

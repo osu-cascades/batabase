72a0f5b39892e222b3c0fc9af65227d7dc69f454
Nathan Struhs <nathan.struhs@getg5.com>
2018-11-21 09:52:22 -0800

    Add user roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
+
+  enum role: [:guest, :field_agent, :admin]
+  after_initialize :set_default_role, if: :new_record?
+
+  private
+
+  def set_default_role
+    self.role ||= :guest
+  end
 end

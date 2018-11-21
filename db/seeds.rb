cd67e7db9c717ca6eb48a5cd58d099043579dd96
Nathan Struhs <nathan.struhs@getg5.com>
2018-11-21 09:58:15 -0800

    Seed each user type

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,39 @@
 User.create!(
-  :email => 'nathanstruhs@gmail.com',
-  :password => 'password',
-  :password_confirmation => 'password',
-  confirmed_at: Time.now.utc
+  email: 'nathanstruhs@gmail.com',
+  password: 'password',
+  password_confirmation: 'password',
+  confirmed_at: Time.now.utc,
+  role: :admin
 )
 
 User.create!(
-  :email => 'drudged@oregonstate.edu',
-  :password => 'password',
-  :password_confirmation => 'password',
-  confirmed_at: Time.now.utc
+  email: 'drudged@oregonstate.edu',
+  password: 'password',
+  password_confirmation: 'password',
+  confirmed_at: Time.now.utc,
+  role: :admin
+)
+
+User.create!(
+  email: 'guest@guest.com',
+  password: 'password',
+  password_confirmation: 'password',
+  confirmed_at: Time.now.utc,
+  role: :guest
+)
+
+User.create!(
+  email: 'field-agent@field-agent.com',
+  password: 'password',
+  password_confirmation: 'password',
+  confirmed_at: Time.now.utc,
+  role: :field_agent
+)
+
+User.create!(
+  email: 'admin@admin.com',
+  password: 'password',
+  password_confirmation: 'password',
+  confirmed_at: Time.now.utc,
+  role: :admin
 )

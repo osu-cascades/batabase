7e863bf4f8635f7794765e41ccad20887a1cf6cc
Nathan Struhs <nathan.struhs@getg5.com>
2018-11-26 10:56:04 -0800

    User model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) { build(:user) }
+
+  describe 'callback' do
+    it '#after_initialize should recieve #set_default_user' do
+      user = User.allocate
+      expect(user).to receive(:set_default_role)
+      user.send(:initialize)
+    end
+  end
+
+  describe '#set_default_role' do
+    it 'sets role to guest' do
+      expect(user.role).to eql('guest')
+    end
+  end
 end

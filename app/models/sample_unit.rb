4d3d87162c9f089472de8c8b2ee002a405e26a03
Nathan Struhs <nathan.struhs@getg5.com>
2018-12-02 17:12:22 -0800

    Sample units add columns

--- app/models/sample_unit.rb
+++ app/models/sample_unit.rb
@@ -2,4 +2,7 @@ class SampleUnit < ApplicationRecord
   validates :site_code, presence: true
   validates_length_of :site_code, maximum: 20
   validates_length_of :federal_agency, maximum: 50
+
+  belongs_to :state_1, class_name: 'State', foreign_key: :state_1_id, optional: true
+  belongs_to :state_2, class_name: 'State', foreign_key: :state_2_id, optional: true
 end
--- spec/models/sample_unit_spec.rb
+++ spec/models/sample_unit_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe SampleUnit, type: :model do
     it { should validate_length_of(:site_code).is_at_most(20) }
     it { should validate_length_of(:federal_agency).is_at_most(50) }
   end
+
+  describe 'associations' do
+    it { should belong_to(:state_1).optional }
+    it { should belong_to(:state_2).optional }
+  end
 end

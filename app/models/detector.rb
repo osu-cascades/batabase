c90dbb3200157f7eb85f5cae1125a37fe85d4b0e
Nathan Struhs <nathan.struhs@getg5.com>
2018-12-02 16:43:16 -0800

    Detector organization assocation

--- app/models/detector.rb
+++ app/models/detector.rb
@@ -1,4 +1,12 @@
 class Detector < ApplicationRecord
   validates :serial_num, :manufacturer, presence: true
   validates_length_of :serial_num, :model, :manufacturer, :firmware, maximum: 50
+
+  belongs_to :organization, optional: true
+
+  protected
+
+  def owning_agency
+    organization.try(:label)
+  end
 end

e4956abf136da214f0e371c60f24225ff47c6a84
Nathan Struhs <nathan.struhs@getg5.com>
2018-12-04 14:30:08 -0800

    Remove simplecov and heroku test configs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,19 +29,3 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
-
-  # Heroku build packs need to put the chromedriver binary in a non-standard location specified by GOOGLE_CHROME_SHIM
-  chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
-
-  options = {}
-  options[:args] = ['headless', 'disable-gpu', 'window-size=1280,1024']
-  options[:binary] = chrome_bin if chrome_bin
-
-  Capybara.register_driver :headless_chrome do |app|
-    Capybara::Selenium::Driver.new(app,
-       browser: :chrome,
-       options: Selenium::WebDriver::Chrome::Options.new(options)
-     )
-  end
-
-  Capybara.javascript_driver = :headless_chrome

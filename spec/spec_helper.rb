581eed7c9c5b438af9dd2e4d8e553ec7ef25bbb5
Nathan Struhs <nathan.struhs@getg5.com>
2018-12-04 14:30:08 -0800

    Remove simplecov and heroku test configs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,3 @@
-require 'simplecov'
-SimpleCov.start do
-  add_filter %r{^/spec/}
-  add_filter %r{^/config/}
-end
-
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true

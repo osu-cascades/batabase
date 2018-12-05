163f18caf066ca4af265885c31c81316a3bb4bd6
Nathan Struhs <nathan.struhs@getg5.com>
2018-12-05 09:06:11 -0800

    Remove uglifier for sprockets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.assets.js_compressor = :uglifier
 end

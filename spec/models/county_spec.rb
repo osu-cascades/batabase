9004965dd3baff1ddaf62691b1c5aac138c51a2b
Nathan Struhs <nathan.struhs@getg5.com>
2019-01-19 12:23:43 -0800

    County: assocation spec

--- spec/models/county_spec.rb
+++ spec/models/county_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe County, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'assocations' do
+    it { should belong_to(:state) }
+  end
 end

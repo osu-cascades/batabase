1c3b8b06fe2be56e463b6b38768ec37b2d2818ba
Nathan Struhs <nathan.struhs@getg5.com>
2019-01-19 12:28:26 -0800

    State: add has_many counties assocation

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,4 +1,6 @@
 class State < ApplicationRecord
+  has_many :counties
+
   validates :state_code, :state_name, presence: true
   validates_length_of :state_code, maximum: 2
   validates_length_of :state_name, maximum: 50

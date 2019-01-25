b5de505d0f8ae932219ac1aa26099bfda4c42bcd
Nathan Struhs <nathan.struhs@getg5.com>
2019-01-25 09:23:19 -0800

    Clean up factories

--- spec/factories/counties.rb
+++ spec/factories/counties.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :county do
-    state { nil }
-    name { "Example County" }
+    state { State.first || association(:state) }
+    name { "Example County Name" }
   end
 end

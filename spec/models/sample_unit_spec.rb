b08110787a0e81aebd04b934b4e3d635e598f0ec
Nathan Struhs <nathan.struhs@getg5.com>
2019-01-25 10:28:11 -0800

    Configure belongs to assocation: SampleUnit -> BroadHabitat

--- spec/models/sample_unit_spec.rb
+++ spec/models/sample_unit_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe SampleUnit, type: :model do
-  subject(:sample_unit) { build(:sample_unit) }
+  subject(:sample_unit) { create(:sample_unit) }
 
   describe 'validations' do
     it { should validate_presence_of(:site_code) }
     it { should validate_length_of(:site_code).is_at_most(20) }
-    it { should validate_length_of(:federal_agency).is_at_most(50) }
+    it { should validate_length_of(:agency).is_at_most(50) }
   end
 
   describe 'associations' do
     it { should belong_to(:state_1).optional }
     it { should belong_to(:state_2).optional }
+    it { should belong_to(:broad_habitat) }
   end
 end

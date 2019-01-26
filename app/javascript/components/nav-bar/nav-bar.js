19431f6f18bb34d91a5c1aceff0fb2642465f412
Nathan Struhs <nathan.struhs@getg5.com>
2019-01-26 12:53:14 -0800

    Show/hide subdropdown menu ietems

--- app/javascript/components/nav-bar/nav-bar.js
+++ app/javascript/components/nav-bar/nav-bar.js
@@ -3,12 +3,28 @@ import './nav-bar.scss';
 class HamburgerMenu {
   constructor() {
     this.burgerListener();
+    this.subDropDownListener();
   }
 
   burgerListener() {
     $('.navbar-burger').click(() => {
       $('.navbar-burger').toggleClass('is-active');
       $('.navbar-menu').toggleClass('is-active');
+    });
+  }
+
+  subDropDownListener() {
+    $('.navbar-link').click((event) => {
+      const target = $(event.currentTarget);
+      if (target.hasClass('is-active')) {
+        // target.find($('.navbar-dropdown')).hide();
+        target.nextAll(".navbar-dropdown:first").hide();
+        target.removeClass('is-active');
+      } else {
+        // target.find($('.navbar-dropdown')).show();
+        target.nextAll(".navbar-dropdown:first").show();
+        target.addClass('is-active');
+      }
     });
   }
 }

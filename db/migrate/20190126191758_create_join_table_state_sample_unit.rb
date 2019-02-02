e14cad8e48f27524c9f21730132892244e3fadc0
Nathan Struhs <nathan.struhs@getg5.com>
2019-02-02 11:41:10 -0800

    Remove state/county has_and_belongs_to_many relationship

--- db/migrate/20190126191758_create_join_table_state_sample_unit.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableStateSampleUnit < ActiveRecord::Migration[5.2]
-  def change
-    create_join_table :states, :sample_units do |t|
-      t.index [:state_id, :sample_unit_id]
-      t.index [:sample_unit_id, :state_id]
-    end
-  end
-end
--- db/migrate/20190126191837_create_join_table_county_sample_unit.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableCountySampleUnit < ActiveRecord::Migration[5.2]
-  def change
-    create_join_table :counties, :sample_units do |t|
-      # t.index [:county_id, :sample_unit_id]
-      # t.index [:sample_unit_id, :county_id]
-    end
-  end
-end

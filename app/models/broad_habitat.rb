49dcba22dc3b327a582e8e5cbb4d9af219b59451
Nathan Struhs <nathan.struhs@getg5.com>
2019-02-21 21:05:23 -0800

    BroadHabitatForm: validations & assocations

--- app/models/broad_habitat.rb
+++ app/models/broad_habitat.rb
@@ -1,4 +1,5 @@
 class BroadHabitat < ApplicationRecord
   validates :label, presence: true
   validates_length_of :label, maximum: 50
+  has_many :broad_habitat_forms
 end
--- app/models/detection_target.rb
+++ app/models/detection_target.rb
@@ -1,5 +1,5 @@
 class DetectionTarget < ApplicationRecord
   has_many :target_descriptors
-
   validates :label, presence: true
+
 end

0d4e4104c02c86c947b654c45a0e3cdc1c381369
Nathan Struhs <nathan.struhs@getg5.com>
2019-02-21 21:08:40 -0800

    Add new broad habitats to db

--- script/createdb.sh
+++ script/createdb.sh
@@ -9,6 +9,7 @@ bundle exec rake seed:detection_targets;
 bundle exec rake seed:target_descriptors;
 bundle exec rake seed:study_areas;
 bundle exec rake seed:county;
+bundle exec rake seed:broad_habitats;
 bundle exec rake seed:sample_units;
 bundle exec rake initialize:contact_state_id;
 bundle exec rake initialize:contact_organization_id;

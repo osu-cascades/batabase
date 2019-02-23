6d6e741bff09037a8bbb58525079df1d08062eb8
Nathan Struhs <nathan.struhs@getg5.com>
2019-02-23 11:41:56 -0800

    Naming.. doiluyn

--- lib/sample_unit_creator.rb
+++ lib/sample_unit_creator.rb
@@ -15,8 +15,8 @@ class SampleUnitCreator
   private
 
   def create_sample_unit(row)
-    broad_id = BroadHabitatForm.find_by(label: row['BroadHabitat_Form']).id
-    new_sample_unit = SampleUnit.new(site_code: row['SampleUnit'], grts: row['GRTS'], broad_habitat_form_id: broad_id)
+    broad_habitat_form_id = BroadHabitatForm.find_by(label: row['BroadHabitat_Form']).id
+    new_sample_unit = SampleUnit.new(site_code: row['SampleUnit'], grts: row['GRTS'], broad_habitat_form_id: broad_habitat_form_id)
     new_sample_unit.save!
 
     create_sample_unit_states(new_sample_unit, row)

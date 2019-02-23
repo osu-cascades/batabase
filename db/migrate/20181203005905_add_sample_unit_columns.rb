0ec62fa0da5fda5fee7ae1558874826c0b2350a4
Nathan Struhs <nathan.struhs@getg5.com>
2019-02-23 11:07:57 -0800

    Fix pluralality of assocation

--- db/migrate/20181203005905_add_sample_unit_columns.rb
+++ db/migrate/20181203005905_add_sample_unit_columns.rb
@@ -2,6 +2,6 @@ class AddSampleUnitColumns < ActiveRecord::Migration[5.2]
   def change
     add_column :sample_units, :grts, :string
     add_reference :sample_units, :species_group, index: true
-    add_reference :sample_units, :broad_habitat_forms, index: true
+    add_reference :sample_units, :broad_habitat_form, index: true
   end
 end

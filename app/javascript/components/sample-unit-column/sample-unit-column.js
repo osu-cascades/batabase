b273d4e7625d5fef783bd873c30d100a820dc568
Nathan Struhs <nathan.struhs@getg5.com>
2019-03-04 09:33:08 -0800

    SampleUnit: driving directions modal

--- app/javascript/components/sample-unit-column/sample-unit-column.js
+++ app/javascript/components/sample-unit-column/sample-unit-column.js
@@ -1,1 +1,27 @@
 import "./sample-unit-column.scss";
+
+class Modal {
+  constructor() {
+    this.modal = $(document).find('.modal');
+    this.button = $(document).find('.modal-button');
+    this.closeModal = $(document).find('.modal-close');
+    this.openModalHandler();
+    this.closeModalHandler();
+  }
+
+  openModalHandler() {
+    this.button.click(event => {
+      this.modal.addClass('is-active');
+    });
+  }
+
+  closeModalHandler() {
+    this.closeModal.click(event => {
+      this.modal.removeClass('is-active');
+    });
+  }
+}
+
+$(document).ready(() => {
+  new Modal();
+});

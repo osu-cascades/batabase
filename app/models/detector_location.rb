ce206dc05328f0eccb6220f05359b81e4fcae110
Nathan Struhs <nathan.struhs@getg5.com>
2019-04-07 10:39:19 -0700

    Fixed #84, Parse location name

--- app/models/detector_location.rb
+++ app/models/detector_location.rb
@@ -11,4 +11,17 @@ class DetectorLocation < ApplicationRecord
   def detection_target_label
     detection_target.try(:label)
   end
+
+  def quad_id
+    location_name.split('_')[1].match(/[a-zA-Z]{2}/)
+  end
+
+  def quad_number
+    qn = location_name.split('_')[1].match(/\d{1}/)
+    qn.nil? ? '1' : qn
+  end
+
+  def sample_unit_id
+    sample_unit.id
+  end
 end

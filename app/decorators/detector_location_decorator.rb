fb05e9efdb6326ef78753fbf765f9a76251b6727
Nathan Struhs <nathan.struhs@getg5.com>
2019-04-14 11:13:20 -0700

    Extract detector location methods into decorator

--- app/decorators/detector_location_decorator.rb
+++ app/decorators/detector_location_decorator.rb
@@ -2,7 +2,28 @@ class DetectorLocationDecorator < ApplicationDecorator
   delegate_all
   decorates :detector_location
 
+  def detection_target_label
+    detection_target.try(:label)
+  end
+
   def coordinates
     "[#{latitude}, #{longitude}]"
+  end
+
+  def quad_id
+    location_name.split('_')[1].match(/[a-zA-Z]{2}/)
+  end
+
+  def quad_number
+    qn = location_name.split('_')[1].match(/\d{1}/)
+    qn.nil? ? '1' : qn
+  end
+
+  def sample_unit_id
+    sample_unit.id
+  end
+
+  def datum
+    geodetic_system.nil? ? 'NAD83' : geodetic_system
   end
 end
--- spec/decorators/detector_location_decorator_spec.rb
+++ spec/decorators/detector_location_decorator_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe DetectorLocationDecorator do
+  let(:detection_target) { build(:detection_target) }
+  let(:detector_location) { build(:detector_location, detection_target: detection_target).decorate }
+
+  describe '#detection_target_label' do
+    it 'returns detection target label' do
+      expect(detector_location.detection_target_label).to eql('Example Detection Target')
+    end
+  end
 end

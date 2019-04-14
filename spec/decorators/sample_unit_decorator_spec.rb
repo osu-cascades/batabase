a71e2f8fff6247d0e93e64bee77255fd0a276d19
Dylan Drudge <drudged@oregonstate.edu>
2019-04-14 11:58:03 -0700

    update sample unit subtitle coverage

--- spec/decorators/sample_unit_decorator_spec.rb
+++ spec/decorators/sample_unit_decorator_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe SampleUnitDecorator do
+  let(:county) { build(:county) }
+  let(:state) { build(:state) }
+  let(:sample_unit) { build(:sample_unit, counties: [county], states: [state] ).decorate }
+
+  describe '#sample_unit_subtitle' do
+    it 'should return a county and state' do 
+      expect(sample_unit.sub_title).to eql('Example County Name, OR')
+    end
+  end
+
 end

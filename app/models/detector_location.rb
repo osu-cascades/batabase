472a9f4c27ead1dcd8f04f6308e64c71f3c3fa2f
Nathan Struhs <nathan.struhs@getg5.com>
2019-04-14 11:13:20 -0700

    Extract detector location methods into decorator

--- app/models/detector_location.rb
+++ app/models/detector_location.rb
@@ -10,24 +10,4 @@ class DetectorLocation < ApplicationRecord
   belongs_to :detection_target, optional: true
   belongs_to :target_descriptor, optional: true
 
-  def detection_target_label
-    detection_target.try(:label)
-  end
-
-  def quad_id
-    location_name.split('_')[1].match(/[a-zA-Z]{2}/)
-  end
-
-  def quad_number
-    qn = location_name.split('_')[1].match(/\d{1}/)
-    qn.nil? ? '1' : qn
-  end
-
-  def sample_unit_id
-    sample_unit.id
-  end
-
-  def datum
-    geodetic_system.nil? ? 'NAD83' : geodetic_system
-  end
 end

0433c9685441d2ee10d6ac2972aa28848744d890
Dylan Drudge <drudged@oregonstate.edu>
2019-04-14 11:35:24 -0700

    update decorator tests to expect non nil name value for contacts

--- spec/decorators/contact_decorator_spec.rb
+++ spec/decorators/contact_decorator_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe ContactDecorator do
+    let(:contact){ build(:contact).decorate }
+
+    describe '#contact_name' do 
+        it 'returns contact name' do
+            expect(contact.name == nil?).to eql(false)
+        end
+    end
 end

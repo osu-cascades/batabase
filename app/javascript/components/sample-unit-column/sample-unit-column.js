68e7626889c4fa1ef76ff4a6c15242aaf075537b
Dylan Drudge <drudged@oregonstate.edu>
2019-04-28 10:57:25 -0700

    add modal class or vewing and editing the image gallery

--- app/javascript/components/sample-unit-column/sample-unit-column.js
+++ app/javascript/components/sample-unit-column/sample-unit-column.js
@@ -1,10 +1,31 @@
 import "./sample-unit-column.scss";
 
-class Modal {
+class DrivingModal {
   constructor() {
-    this.modal = $(document).find('.modal');
-    this.button = $(document).find('.modal-button');
-    this.closeModal = $(document).find('.modal-close');
+    this.modal = $(document).find('.driving-modal');
+    this.button = $(document).find('.driving-button');
+    this.closeModal = $(document).find('.driving-close');
+    this.openModalHandler();
+    this.closeModalHandler();
+  }
+
+  openModalHandler() {
+    this.button.click(event => {
+      this.modal.addClass('is-active');
+    });
+  }
+
+  closeModalHandler() {
+    this.closeModal.click(event => {
+      this.modal.removeClass('is-active');
+    });
+  }
+}
+class GalleryModal {
+  constructor() {
+    this.modal = $(document).find('.gallery-modal');
+    this.button = $(document).find('.gallery-button');
+    this.closeModal = $(document).find('.gallery-close');
     this.openModalHandler();
     this.closeModalHandler();
   }
@@ -23,5 +44,6 @@ class Modal {
 }
 
 $(document).ready(() => {
-  new Modal();
+  new DrivingModal();
+  new GalleryModal();
 });

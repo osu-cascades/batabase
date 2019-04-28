4ad4d016a852a1e5137199f93f2c2ac7e38daf61
Dylan Drudge <drudged@oregonstate.edu>
2019-04-28 12:56:51 -0700

    delete sotrage

--- app/javascript/components/sample-unit-column/sample-unit-column.js
+++ app/javascript/components/sample-unit-column/sample-unit-column.js
@@ -48,7 +48,7 @@ class GalleryModal {
   }
   buildGallery() {
     if (this.imageURLs.length > 0) {
-      this.container.find('img').attr('src', this.imageURLs[0]);;
+      this.container.find('img').attr('src', this.imageURLs[0]);
     }
   }
   closeModalHandler() {
@@ -63,64 +63,25 @@ class SlideShow {
     this.container = $(`#${id}`);
     this.currentIndex = 0;
     this.imageURLs = this.getImageURLs();
-
     this.initialize();
-    this.prev = this.container.find('.prev');
-    this.next = this.container.find('.next');
-
-    this.prevHandler();
-    this.nextHandler();
   }
 
   getImageURLs() {
     return this.container.find('.gallery-image').map((_, element) => {
-      return $(element).data('image-url');
+      return $(element).data('image-url')
     }).get();
   }
 
   initialize() {
     if (this.imageURLs.length > 0) {
-      this.container.append(`
-        <div class='card-image'>
-          <figure class='image is-4by3'>
-            <img src='${this.imageURLs[this.currentIndex]}' />
-          </figure>
-          <i class="slideshow-chevron prev fa fa-2x fa-chevron-left"></i>
-          <i class="slideshow-chevron next fa fa-2x fa-chevron-right"></i>
-        </div>
-      `)
+      this.container.find('img').attr('src', this.imageURLs[this.currentIndex]);
     }
-  }
-
-  prevHandler() {
-    this.prev.on('click', () => {
-      if (this.currentIndex === 0) {
-        this.currentIndex = this.imageURLs.length - 1
-      } else {
-        this.currentIndex--;
-      }
-
-      this.container.find('img').attr('src', this.imageURLs[this.currentIndex]);
-    })
-  }
-
-  nextHandler() {
-    this.next.on('click', () => {
-      if (this.currentIndex === this.imageURLs.length - 1) {
-        this.currentIndex = 0;
-      } else {
-        this.currentIndex++;
-      }
-
-      this.container.find('img').attr('src', this.imageURLs[this.currentIndex]);
-    })
   }
 }
 
 $(document).ready(() => {
   new DrivingDirectionsModal();
-  new GalleryModal();
-
+  
   $('.slideshow').each((_, element) => {
     new SlideShow($(element).attr('id'));
   });

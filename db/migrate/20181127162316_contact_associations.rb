b60c41f361f6b21e65749363b73ce897240b5e4b
Bryce Graves <brycegraves97@gmail.com>
2019-10-22 12:29:32 -0700

    DatabaseMigrations: chop out all migrations in favor of single schema build
    I nuked all of the migrations for the db since they were based on it being prebuilt wrong in favor of using one migration based on the current table schema. This is 100000% worst practices, but since this isn't running in production we should be fine.

--- db/migrate/20181127162316_contact_associations.rb
+++
@@ -1,6 +0,0 @@
-class ContactAssociations < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :contacts, :state, index: true
-    add_reference :contacts, :organization, index: true
-  end
-end
--- db/migrate/20190407183748_add_target_descriptorto_detectorlocations.rb
+++
@@ -1,5 +0,0 @@
-class AddTargetDescriptortoDetectorlocations < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :detector_locations, :target_descriptor, foreign_key: true
-  end
-end

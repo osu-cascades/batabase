0ce213b3aae820a104c235eafd27a41cd7cd855a
Bryce Graves <brycegraves97@gmail.com>
2019-10-25 11:38:57 -0700

    Manifest: add wildcard for autogen chaff in logo

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,5 @@
 // JS and CSS bundles
 //
 //= app/javascript ../javascripts .js
-// = link rails_db/logo.png
-// = link rails_db/logo_mini.png
+// = link rails_db/logo*.png
+// = link rails_db/logo_mini*.png

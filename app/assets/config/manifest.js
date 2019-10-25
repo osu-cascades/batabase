9098f0d5cacad53ec1ae08f1566b21320be6f6b1
Mackenzie Hatfield <mackhatfield@10-116-13-12.wireless.oregonstate.edu>
2019-10-25 10:14:08 -0700

    Link in rails_db assets in manifest.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,7 @@
 // JS and CSS bundles
 //
-//= link rails_db/logo.png
-//= link rails_db/logo_mini.png
 //= app/javascript ../javascripts .js
+// = link rails_db/logo.png
+// = link rails_db/logo_mini.png
+
 

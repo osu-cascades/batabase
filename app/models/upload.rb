1d0ee6427eeec42ef49f471eb001e4739eda56ac
Bryce Graves <brycegraves97@gmail.com>
2019-11-11 13:13:33 -0800

    Upload: add validation to upload model

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,3 +1,4 @@
 class Upload < ApplicationRecord
   validates :data, presence: true, allow_blank: false
+  validates :type, presence: true, allow_blank: false, inclusion: { in: [ "meta", "sono" ]
 end

d1769f3917075f61154ef88d5ce9afa35c80cdaa
Bryce Graves <brycegraves97@gmail.com>
2019-11-11 13:37:55 -0800

    Migrations: change upload's type to upload_type since type is reserved

--- db/migrate/20191110235901_create_uploads.rb
+++ db/migrate/20191110235901_create_uploads.rb
@@ -2,7 +2,7 @@ class CreateUploads < ActiveRecord::Migration[6.0]
   def change
     create_table :uploads do |t|
       t.string :data
-      t.string :type
+      t.string :upload_type
     end
   end
 end

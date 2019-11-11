4d46b895b55fd6056acaa455cb5f654cfe493fbf
Bryce Graves <brycegraves97@gmail.com>
2019-11-11 13:37:21 -0800

    Upload: change type to upload_type since type is reserved

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,4 +1,4 @@
 class Upload < ApplicationRecord
   validates :data, presence: true, allow_blank: false
-  validates :type, presence: true, allow_blank: false, inclusion: { in: [ "meta", "sono" ]
+  validates :upload_type, presence: true, allow_blank: false, inclusion: { in: [ "meta", "sono" ] }
 end

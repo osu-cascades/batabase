9c66999fbef93741f10d0df8543f55c7afbbccdb
Bryce Graves <brycegraves97@gmail.com>
2019-11-11 13:10:57 -0800

    UploadSpec: add failing test for new upload type

--- spec/models/upload_spec.rb
+++ spec/models/upload_spec.rb
@@ -11,11 +11,27 @@ RSpec.describe Upload do
 
         expect(valid_upload).to be_valid
       end
+
+      it "It has a type of meta or sono" do
+        valid_upload.type = "meta"
+
+        expect(valid_upload).to be_valid
+
+        valid_upload.type = "sono"
+
+        expect(valid_upload).to be_valid
+      end
     end
 
     context "A upload is invalid if" do
       it "It contains no data" do
         invalid_upload.data = ""
+
+        expect(invalid_upload).to_not be_valid
+      end
+
+      it "It has a type that isn't meta or sono" do
+        invalid_upload.type = "FAKE TYPE"
 
         expect(invalid_upload).to_not be_valid
       end

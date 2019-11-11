c701b56743a680f85fb150a89f84982f9f01466f
Bryce Graves <brycegraves97@gmail.com>
2019-11-11 00:13:10 -0800

    Components: add component structure and global header

---
+++ app/helpers/components_helper.rb
@@ -0,0 +1,35 @@
+module ComponentsHelper
+  # Main component call
+  def component(name, *options, &block)
+    render("components/#{name}", *options, &block)
+  end
+
+  # All components
+
+  def global_header(*options, &block)
+    component(:global_header, *options, &block)
+  end
+
+  # These are component calls for bootstrap popover and tooltips.
+  # We aren't using them yet but we are just keeping them around.
+  # def popover(content, title: '', **options)
+  #   {
+  #     data: {
+  #       toggle: :popover,
+  #       content: content,
+  #       title: title,
+  #       **options,
+  #     }
+  #   }
+  # end
+
+  # def tooltip(title, **options)
+  #   {
+  #     title: title,
+  #     data: {
+  #       toggle: :tooltip,
+  #       **options
+  #     }
+  #   }
+  # end
+end

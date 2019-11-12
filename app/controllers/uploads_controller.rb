570f8dddf4c6b51340ccb81366ddedb598b11c4c
Mackenzie Hatfield <mackhatfield@MacBook-Pro.local>
2019-11-11 21:00:32 -0800

    Upload: parse params from upload for Upload fields. Have option values match true values on model.

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -12,9 +12,15 @@ class UploadsController < ApplicationController
   end
 
   def create
-    upload_data = params[:game][:data]
+    upload_data = params[:upload][:data]
+    file_name = upload_data.original_filename
+    upload_type = params[:upload][:upload_type]
+
+    byebug
     @upload = Upload.create(
+      file_name: file_name,
       data: upload_data,
+      upload_type: upload_type
     )
 
     if @upload.errors.any?

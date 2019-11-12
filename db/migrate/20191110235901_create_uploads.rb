d1984b7fcf578a6bf928d6b575d435e66ff53b52
Mackenzie Hatfield <mackhatfield@MacBook-Pro.local>
2019-11-11 20:26:52 -0800

    Upload: run migration to add file name member.

--- db/migrate/20191110235901_create_uploads.rb
+++ db/migrate/20191110235901_create_uploads.rb
@@ -1,6 +1,7 @@
 class CreateUploads < ActiveRecord::Migration[6.0]
   def change
     create_table :uploads do |t|
+      t.string :file_name
       t.string :data
       t.string :upload_type
     end

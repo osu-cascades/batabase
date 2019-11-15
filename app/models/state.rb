af24a81c36835024c1d03e9ec01d3788a248202d
Bryce Graves <brycegraves97@gmail.com>
2019-11-15 11:21:10 -0800

    Models: update county and state models
    I added associations between these models.
    
    A state has many counties.
    A county belongs to a state and will be deleted if we delete it

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,4 +1,5 @@
 class State < ApplicationRecord
   validates :name, presence: true, allow_blank: false
   validates :abbreviation, presence: true, allow_blank: false
+  has_many :counties, dependent: :destroy
 end

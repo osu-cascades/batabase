205b1295efcee7889f3f6c7fab8c5e83d479404e
Bryce Graves <brycegraves97@gmail.com>
2019-11-15 12:51:35 -0800

    Seeds: update state and county seeds to use csv imports

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,62 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "'Star Wars' }, abbreviation:  { name: "'Lord of the Rings' }])
-#   Character.create(name: "'Luke', abbreviation:  movie: movies.first)
+require 'activerecord-import/base'
 
-State.create(
-  [
-    { name: "Alabama", abbreviation: "AL"},
-    { name: "Alaska", abbreviation: "AK"},
-    { name: "Arizona", abbreviation: "AZ"},
-    { name: "Arkansas", abbreviation: "AR"},
-    { name: "California", abbreviation: "CA"},
-    { name: "Colorado", abbreviation: "CO"},
-    { name: "Connecticut", abbreviation: "CT"},
-    { name: "Delaware", abbreviation: "DE"},
-    { name: "Florida", abbreviation: "FL"},
-    { name: "Georgia", abbreviation: "GA"},
-    { name: "Hawaii", abbreviation: "HI"},
-    { name: "Idaho", abbreviation: "ID"},
-    { name: "Illinois", abbreviation: "IL"},
-    { name: "Indiana", abbreviation: "IN"},
-    { name: "Iowa", abbreviation: "IA"},
-    { name: "Kansas", abbreviation: "KS"},
-    { name: "Kentucky", abbreviation: "KY"},
-    { name: "Louisiana", abbreviation: "LA"},
-    { name: "Maine", abbreviation: "ME"},
-    { name: "Maryland", abbreviation: "MD"},
-    { name: "Massachusetts", abbreviation: "MA"},
-    { name: "Michigan", abbreviation: "MI"},
-    { name: "Minnesota", abbreviation: "MN"},
-    { name: "Mississippi", abbreviation: "MS"},
-    { name: "Missouri", abbreviation: "MO"},
-    { name: "Montana", abbreviation: "MT"},
-    { name: "Nebraska", abbreviation: "NE"},
-    { name: "Nevada", abbreviation: "NV"},
-    { name: "New Hampshire", abbreviation: "NH"},
-    { name: "New Jersey", abbreviation: "NJ"},
-    { name: "New Mexico", abbreviation: "NM"},
-    { name: "New York", abbreviation: "NY"},
-    { name: "North Carolina", abbreviation: "NC"},
-    { name: "North Dakota", abbreviation: "ND"},
-    { name: "Ohio", abbreviation: "OH"},
-    { name: "Oklahoma", abbreviation: "OK"},
-    { name: "Oregon", abbreviation: "OR"},
-    { name: "Pennsylvania", abbreviation: "PA"},
-    { name: "Rhode Island", abbreviation: "RI"},
-    { name: "South Carolina", abbreviation: "SC"},
-    { name: "South Dakota", abbreviation: "SD"},
-    { name: "Tennessee", abbreviation: "TN"},
-    { name: "Texas", abbreviation: "TX"},
-    { name: "Utah", abbreviation: "UT"},
-    { name: "Vermont", abbreviation: "VT"},
-    { name: "Virginia", abbreviation: "VA"},
-    { name: "Washington", abbreviation: "WA"},
-    { name: "West Virginia", abbreviation: "WV"},
-    { name: "Wisconsin", abbreviation: "WI"},
-    { name: "Wyoming", abbreviation: "WY"},
-  ]
-)
+# load the appropriate database adapter (postgresql, mysql2, sqlite3, etc)
+require 'activerecord-import/active_record/adapters/postgresql_adapter'
+require 'csv'
+
+# Import state data from csv
+columns = [:name, :abbreviation]
+values = CSV.read(Rails.root.join('db/data/states.csv'))
+
+State.import columns, values
+
+# Import county data from csv
+columns = [:state_id, :name]
+values = CSV.read(Rails.root.join('db/data/counties.csv'))
+
+County.import columns, values

34d61ba92b8fe659ce209e2c8793a243c6674dba
Bryce Graves <brycegraves97@gmail.com>
2019-11-15 09:30:05 -0800

    StateSpec: add tests for new state model

---
+++ spec/models/state_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+RSpec.describe State do
+  let(:state) { FactoryBot.build_stubbed(:state) }
+
+  describe "State Validation" do
+    context "A state is valid if" do
+      it "It has a name" do
+        state.name = "FAKE NEME"
+
+        expect(state).to be_valid
+      end
+
+      it "It has an abbreviation" do
+        state.abbreviation = "FAKE ABBREVIATION"
+
+        expect(state).to be_valid
+      end
+    end
+
+    context "A state is invalid if" do
+      it "It has no name" do
+        state.name = ""
+
+        expect(state).to_not be_valid
+      end
+
+      it "It has no abbreviation" do
+        state.abbreviation = ""
+
+        expect(state).to_not be_valid
+      end
+    end
+  end
+
+end

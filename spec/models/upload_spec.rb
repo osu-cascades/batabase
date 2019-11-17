a4a68fbc6564ba6c2741838cf6892fcc5ccfafd8
Bryce Graves <brycegraves97@gmail.com>
2019-11-16 19:39:56 -0800

    Upload: reorder spec to match model validation order

--- spec/models/upload_spec.rb
+++ spec/models/upload_spec.rb
@@ -5,15 +5,14 @@ RSpec.describe Upload do
 
   describe "Upload Validation" do
     context "An upload is valid if" do
-      it "It has a filename" do
-        upload.filename = "FAKE_FILENAME.csv"
+      it "It contains data" do
+        upload.data = "FAKE DATA"
 
         expect(upload).to be_valid
       end
 
-
-      it "It contains data" do
-        upload.data = "FAKE DATA"
+      it "It has a filename" do
+        upload.filename = "FAKE_FILENAME.csv"
 
         expect(upload).to be_valid
       end

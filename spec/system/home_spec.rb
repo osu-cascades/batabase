ec929dada9f0706db533cf90931130b9b8eb6545
Bryce Graves <brycegraves97@gmail.com>
2019-11-17 01:22:10 -0800

    HomeSpec: add spec for visiting home page

---
+++ spec/system/home_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'Home Flows', type: :system do
+  context 'View Home Page Flow' do
+    it 'A user can view home page' do
+      visit home_index_path
+      
+      expect(page).to have_content('Batabase')
+    end
+  end
+end

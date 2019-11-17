758593f80e09e4f428e6e6ef03ab17e85f39f675
Bryce Graves <brycegraves97@gmail.com>
2019-11-16 19:09:32 -0800

    Schema: update to match new target descriptor migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_17_011807) do
+ActiveRecord::Schema.define(version: 2019_11_17_030721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,10 +30,17 @@ ActiveRecord::Schema.define(version: 2019_11_17_011807) do
     t.string "abbreviation"
   end
 
+  create_table "target_descriptors", force: :cascade do |t|
+    t.string "label"
+    t.bigint "detection_target_id", null: false
+    t.index ["detection_target_id"], name: "index_target_descriptors_on_detection_target_id"
+  end
+
   create_table "uploads", force: :cascade do |t|
     t.string "data"
     t.string "upload_type"
   end
 
   add_foreign_key "counties", "states"
+  add_foreign_key "target_descriptors", "detection_targets"
 end

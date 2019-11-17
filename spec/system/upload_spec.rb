20a7e7395eaae69dde97b8ae54d8cd5fee08bac7
Bryce Graves <brycegraves97@gmail.com>
2019-11-17 15:08:22 -0800

    UploadSpec: add js tag for only tests that require it

--- spec/system/upload_spec.rb
+++ spec/system/upload_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 GENERATED_UPLOAD_COUNT = 3
 
-RSpec.describe 'Upload Flows', type: :system, js: true do
+RSpec.describe 'Upload Flows', type: :system do
   describe "Index Flows" do
     context 'View Uploads' do
       before(:each) do
@@ -14,7 +14,7 @@ RSpec.describe 'Upload Flows', type: :system, js: true do
         1.upto(GENERATED_UPLOAD_COUNT) { |i| expect(page).to have_content("FAKE_FILENAME_#{i}.csv") }
       end
 
-      it 'A user can click to view an uploads data' do
+      it 'A user can click to view an uploads data', js:true do
         first('a', text: 'View Upload').click
 
         expect(page).to have_content("FAKE_FILENAME_1.csv")

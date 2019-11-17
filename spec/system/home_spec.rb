8f67441a53a0ad335aebcc52bc2c998a315fd8d4
Bryce Graves <brycegraves97@gmail.com>
2019-11-17 13:02:40 -0800

    SystemSpecs: refactor the structure of system tests moving forward

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Home Flows', type: :system do
-  context 'View Home Page Flow' do
-    it 'A user can view home page' do
-      visit home_index_path
-      
-      expect(page).to have_content('Batabase')
+  describe "Index Flows" do
+    context 'View Home Page Flow' do
+      it 'A user can view home page' do
+        visit home_index_path
+        
+        expect(page).to have_content('Batabase')
+      end
     end
   end
 end
--- spec/system/upload_spec.rb
+++ spec/system/upload_spec.rb
@@ -3,14 +3,16 @@ require 'rails_helper'
 GENERATED_UPLOAD_COUNT = 3
 
 RSpec.describe 'Upload Flows', type: :system do
-  context 'View Uploads Flow' do
-    before(:each) do
-      create_list(:upload, GENERATED_UPLOAD_COUNT, :meta)
-      visit uploads_path
-    end
+  describe "Index Flows" do
+    context 'View Uploads' do
+      before(:each) do
+        create_list(:upload, GENERATED_UPLOAD_COUNT, :meta)
+        visit uploads_path
+      end
 
-    it 'A user can view all uploads' do
-      1.upto(GENERATED_UPLOAD_COUNT) { |i| expect(page).to have_content("FAKE_FILENAME_#{i}.csv") }
+      it 'A user can view all uploads' do
+        1.upto(GENERATED_UPLOAD_COUNT) { |i| expect(page).to have_content("FAKE_FILENAME_#{i}.csv") }
+      end
     end
   end
 end

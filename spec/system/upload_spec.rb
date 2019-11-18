70671da19197ee52bf7f32fbcfd4729189e16f17
Bryce Graves <brycegraves97@gmail.com>
2019-11-17 16:39:00 -0800

    UploadSpec: refactor structure

--- spec/system/upload_spec.rb
+++ spec/system/upload_spec.rb
@@ -3,40 +3,34 @@ require 'rails_helper'
 GENERATED_UPLOAD_COUNT = 3
 
 RSpec.describe 'Upload Flows', type: :system do
-  describe "Index Flows" do
-    context 'View Uploads' do
-      before(:each) do
-        create_list(:upload, GENERATED_UPLOAD_COUNT, :meta)
-        visit uploads_path
-      end
-
-      it 'A user can view all uploads' do
-        1.upto(GENERATED_UPLOAD_COUNT) { |i| expect(page).to have_content("FAKE_FILENAME_#{i}.csv") }
-      end
-
-      it 'A user can click to view the data of an upload', js:true do
-        first('a', text: 'View Upload').click
-
-        expect(page).to have_content("FAKE_FILENAME_1.csv")
-      end
-
-      it 'A user can click to delete an upload', js:true do
-        expect(page).to have_content("FAKE_FILENAME_1.csv")
-
-        first('form').click
-
-        expect(page).to_not have_content("FAKE_FILENAME_1.csv")
-      end
-
-      it 'A user can click to navigate to new upload page', js:true do
-        find('a', text: 'Add Upload').click
-
-        expect(page).to_not have_content("FAKE_FILENAME_1.csv")
-      end
+  context 'Index' do
+    before(:each) do
+      create_list(:upload, GENERATED_UPLOAD_COUNT, :meta)
+      visit uploads_path
     end
-  end
 
-  describe "New Flow" do
-    
+    it 'A user can view all uploads' do
+      1.upto(GENERATED_UPLOAD_COUNT) { |i| expect(page).to have_content("FAKE_FILENAME_#{i}.csv") }
+    end
+
+    it 'A user can click to view the data of an upload', js:true do
+      first('a', text: 'View Upload').click
+
+      expect(page).to have_content("FAKE_FILENAME_1.csv")
+    end
+
+    it 'A user can click to delete an upload', js:true do
+      expect(page).to have_content("FAKE_FILENAME_1.csv")
+
+      first('form').click
+
+      expect(page).to_not have_content("FAKE_FILENAME_1.csv")
+    end
+
+    it 'A user can click to navigate to new upload page', js:true do
+      find('a', text: 'Add Upload').click
+
+      expect(page).to_not have_content("FAKE_FILENAME_1.csv")
+    end
   end
 end

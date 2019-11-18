3b19a5517ba0ff60e87cac73c2f06884281bcdb2
Bryce Graves <brycegraves97@gmail.com>
2019-11-17 16:39:47 -0800

    HomeSpec: refactor structure

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -1,15 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Home Flows', type: :system do
-  describe "Index Flows" do
-    context 'View Home Page Flow' do
-      it 'A user can view home page' do
-        visit home_index_path
+  context 'Index' do
+    it 'A user can view home page' do
+      visit home_index_path
 
-        page_title = find_by_id('home-title')
+      page_title = find_by_id('home-title')
 
-        expect(page_title).to have_content('Batabase')
-      end
+      expect(page_title).to have_content('Batabase')
     end
   end
 end

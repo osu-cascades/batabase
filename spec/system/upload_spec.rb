783bc0ab7fd6b523c0d6846b5bffa098fcb6291d
Bryce Graves <brycegraves97@gmail.com>
2019-11-17 16:35:22 -0800

    UploadSpec: add tests for the remaining clickable options on index

--- spec/system/upload_spec.rb
+++ spec/system/upload_spec.rb
@@ -14,10 +14,24 @@ RSpec.describe 'Upload Flows', type: :system do
         1.upto(GENERATED_UPLOAD_COUNT) { |i| expect(page).to have_content("FAKE_FILENAME_#{i}.csv") }
       end
 
-      it 'A user can click to view an uploads data', js:true do
+      it 'A user can click to view the data of an upload', js:true do
         first('a', text: 'View Upload').click
 
         expect(page).to have_content("FAKE_FILENAME_1.csv")
+      end
+
+      it 'A user can click to delete an upload', js:true do
+        expect(page).to have_content("FAKE_FILENAME_1.csv")
+
+        first('form').click
+
+        expect(page).to_not have_content("FAKE_FILENAME_1.csv")
+      end
+
+      it 'A user can click to navigate to new upload page', js:true do
+        find('a', text: 'Add Upload').click
+
+        expect(page).to_not have_content("FAKE_FILENAME_1.csv")
       end
     end
   end

e46b8def27da35387dded3027ba1d850e51f823c
Bryce Graves <brycegraves97@gmail.com>
2019-11-21 03:12:17 -0800

    BroadHabitat: refactor migration

--- db/migrate/20191121092350_create_broad_habitat.rb
+++ db/migrate/20191121092350_create_broad_habitat.rb
@@ -1,8 +1,7 @@
 class CreateBroadHabitat < ActiveRecord::Migration[6.0]
   def change
     create_table :broad_habitats do |t|
-      t.string :habitat_class
-      t.string :habitat_form
+      t.string :label
     end
   end
 end

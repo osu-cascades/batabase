6818d57ef9149c19aa2b204d20e39d4a6bedef79
Bryce Graves <brycegraves97@gmail.com>
2019-11-21 03:11:40 -0800

    BroadHabitat: refactor spec

--- spec/models/broad_habitat_spec.rb
+++ spec/models/broad_habitat_spec.rb
@@ -5,14 +5,8 @@ RSpec.describe BroadHabitat do
 
   describe "Broad Habitat Validation" do
     context "An broad habitat is valid if" do
-      it "It has a habitat class" do
-        broad_habitat.habitat_class = "FAKE CLASS"
-
-        expect(broad_habitat).to be_valid
-      end
-
-      it "It has a habitat form" do
-        broad_habitat.habitat_form = "FAKE FORM"
+      it "It has a habitat label" do
+        broad_habitat.label = "FAKE LABEL"
 
         expect(broad_habitat).to be_valid
       end
@@ -20,13 +14,7 @@ RSpec.describe BroadHabitat do
 
     context "An broad habitat is invalid if" do
       it "It has no habitat class" do
-        broad_habitat.habitat_class = ""
-
-        expect(broad_habitat).to_not be_valid
-      end
-
-      it "It has no habitat form" do
-        broad_habitat.habitat_form = ""
+        broad_habitat.label = ""
 
         expect(broad_habitat).to_not be_valid
       end

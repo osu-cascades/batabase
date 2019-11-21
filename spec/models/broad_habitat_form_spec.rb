0e6818d48f10ed31d42b35468da3d3cebef427e4
Bryce Graves <brycegraves97@gmail.com>
2019-11-21 03:11:02 -0800

    BroadHabitatForm: add spec

---
+++ spec/models/broad_habitat_form_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe BroadHabitatForm do
+  let(:broad_habitat_form) { FactoryBot.build_stubbed(:broad_habitat_form) }
+
+  describe "Broad Habitat Validation" do
+    context "An broad habitat is valid if" do
+      it "It has a habitat label" do
+        broad_habitat_form.label = "FAKE LABEL"
+
+        expect(broad_habitat_form).to be_valid
+      end
+    end
+
+    context "An broad habitat is invalid if" do
+      it "It has no habitat class" do
+        broad_habitat_form.label = ""
+
+        expect(broad_habitat_form).to_not be_valid
+      end
+    end
+  end
+
+end

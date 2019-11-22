7f054a9297abf5d6af9c7b02b579dfca271f3b70
Bryce Graves <brycegraves97@gmail.com>
2019-11-22 13:56:38 -0800

    State: add state with counties to factory

--- spec/factories/states.rb
+++ spec/factories/states.rb
@@ -2,5 +2,15 @@ FactoryBot.define do
   factory :state do
     name { 'FAKE NAME' }
     abbreviation { 'FAKE ABBREVIATION' }
+
+    trait :has_counties do
+      counties do
+        Array.new(5) do |i|
+          County.new(
+            name: "FAKE NAME #{i.humanize.upcase}"
+          )
+        end
+      end
+    end
   end
 end

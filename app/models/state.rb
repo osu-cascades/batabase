04c87fca4aa664acece7fa150c2b0dfa88708d68
Bryce Graves <brycegraves97@gmail.com>
2019-11-22 13:57:08 -0800

    State: add methods for checking if a state has counties

--- app/models/state.rb
+++ app/models/state.rb
@@ -2,4 +2,8 @@ class State < ApplicationRecord
   validates :name, presence: true, allow_blank: false
   validates :abbreviation, presence: true, allow_blank: false
   has_many :counties, dependent: :destroy
+
+  def counties?
+    !counties.empty?
+  end
 end

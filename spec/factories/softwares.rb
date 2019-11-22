6f6d004ab5ff3c957982526ff7cc9ff4cd147926
Bryce Graves <brycegraves97@gmail.com>
2019-11-22 10:55:53 -0800

    Software: add factory

---
+++ spec/factories/softwares.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :software do
+    trait :sonobat do
+      name { 'Sonobat 4' }
+    end
+
+    trait :Kaleidoscope do
+      name { 'Kaleidoscope' }
+    end
+  end
+end

69e6b4b5237b267090a7575d21e4f6d691992d77
Bryce Graves <brycegraves97@gmail.com>
2019-11-23 01:04:41 -0800

    County: add sample unit associations to model

--- app/models/county.rb
+++ app/models/county.rb
@@ -1,5 +1,9 @@
 class County < ApplicationRecord
   validates :name, presence: true, allow_blank: false
+
+  has_many :sample_unit_counties
+  has_many :sample_units, through: :sample_unit_counties
+
   belongs_to :state
 
   def get_state

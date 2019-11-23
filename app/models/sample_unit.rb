96d6eb54dc5026b4c7c132e5f9742f0e1cbb7bed
Bryce Graves <brycegraves97@gmail.com>
2019-11-23 01:06:59 -0800

    SampleUnit: add sample unit model

---
+++ app/models/sample_unit.rb
@@ -0,0 +1,27 @@
+class SampleUnit < ApplicationRecord
+  validate :agency
+  validates :grts, numericality: { only_integer: true }
+
+  # TODO: Detector locations
+  # has_many :detector_locations, dependent: :destroy
+  # accepts_nested_attributes_for :detector_locations, allow_destroy: true
+
+  belongs_to :broad_habitat_form, optional: true
+
+  has_many :sample_unit_states
+  has_many :sample_unit_counties
+
+  # TODO: figure out if these are needed
+  has_many :states, through: :sample_unit_states
+  has_many :counties, through: :sample_unit_counties
+
+  def primary_state
+    # TODO: this
+  end
+
+  def primary_county
+    # TODO: this
+  end
+
+  # TODO: add methods for pulling data about states and counties and their percents
+end

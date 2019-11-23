784857cfeaacc98fcac0344ad2aab5f0c824b88b
Bryce Graves <brycegraves97@gmail.com>
2019-11-22 21:26:51 -0800

    Orientation: add factory

---
+++ spec/factories/orientations.rb
@@ -0,0 +1,35 @@
+FactoryBot.define do
+  factory :orientation do
+    trait :north do
+      direction { 'N' }
+    end
+
+    trait :west do
+      direction { 'W' }
+    end
+
+    trait :east do
+      direction { 'E' }
+    end
+
+    trait :south do
+      direction { 'S' }
+    end
+
+    trait :northwest do
+      direction { 'NW' }
+    end
+
+    trait :northeast do
+      direction { 'NE' }
+    end
+
+    trait :southwest do
+      direction { 'SW' }
+    end
+
+    trait :southeast do
+      direction { 'SE' }
+    end
+  end
+end

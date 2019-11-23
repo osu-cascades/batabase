e1787264b0f22b166f283e62cf9be5479757f988
Bryce Graves <brycegraves97@gmail.com>
2019-11-23 01:04:55 -0800

    State: add sample unit associations to model

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,7 +1,12 @@
 class State < ApplicationRecord
-  validates :name, presence: true, allow_blank: false
+  # TODO: add length check
   validates :abbreviation, presence: true, allow_blank: false
+  validates :name, presence: true, allow_blank: false
+
   has_many :counties, dependent: :destroy
+  has_many :sample_unit_states
+  has_many :sample_units, through: :sample_unit_states
+
 
   def counties?
     !counties.empty?

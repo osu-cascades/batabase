42631801acb00fc907779f30019529b75555e82a
Bryce Graves <brycegraves97@gmail.com>
2020-01-25 02:36:02 -0800

    Routes: format via rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root to: redirect(path: '/home', status: 302)
 
-  resources :home, only: [ :index ]
+  resources :home, only: [:index]
   resources :uploads do
     get :commit
   end

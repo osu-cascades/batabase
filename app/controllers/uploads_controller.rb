0b121f183a761e8072ee3fe798cc95a1c41b506e
Bryce Graves <brycegraves97@gmail.com>
2020-01-25 02:42:06 -0800

    UploadsController: fix unused variable warning

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -47,7 +47,7 @@ class UploadsController < ApplicationController
   end
 
   def commit
-    upload = Upload.find(params[:upload_id])
+    @upload = Upload.find(params[:upload_id])
     # This will be where the work happens for turning
     # the CSV into tables in db
     redirect_to uploads_path

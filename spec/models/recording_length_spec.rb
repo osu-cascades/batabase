7d0c27bbceadb34fd9d1dbf6a8f27e3e37ae75fd
Bryce Graves <brycegraves97@gmail.com>
2020-01-25 01:59:38 -0800

    RecordingLengthSpec: format via rubocop

--- spec/models/recording_length_spec.rb
+++ spec/models/recording_length_spec.rb
@@ -1,10 +1,12 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe RecordingLength, type: :model do
   let(:recording_length) { FactoryBot.build_stubbed(:recording_length) }
 
-  describe "Recording Length Validation" do
-    context "A recording length is valid if" do
+  describe 'Recording Length Validation' do
+    context 'A recording length is valid if' do
       it "It has a length that's a float" do
         recording_length.length = 4.2
 
@@ -12,23 +14,22 @@ RSpec.describe RecordingLength, type: :model do
       end
     end
 
-    context "A recording length is invalid if" do
+    context 'A recording length is invalid if' do
       it "It has a length that isn't a float" do
         recording_length.length = nil
 
         expect(recording_length).to_not be_valid
 
-        recording_length.length = "INVALID"
+        recording_length.length = 'INVALID'
 
         expect(recording_length).to_not be_valid
       end
 
-      it "It has a length of zero" do
+      it 'It has a length of zero' do
         recording_length.length = 0
 
         expect(recording_length).to_not be_valid
       end
     end
   end
-
 end

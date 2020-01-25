24de9665443d20498dae5c1c7e5769ed5b1ddfe0
Bryce Graves <brycegraves97@gmail.com>
2020-01-25 01:21:19 -0800

    Detectors: format via rubocop

--- spec/factories/detectors.rb
+++ spec/factories/detectors.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :detector do
     organization
-    manufacturer { "FAKE MANUFACTURER" }
-    model { "FAKE MODEL" }
-    serial_number { "FAKE SERIAL NUMBER" }
+    manufacturer { 'FAKE MANUFACTURER' }
+    model { 'FAKE MODEL' }
+    serial_number { 'FAKE SERIAL NUMBER' }
   end
 end

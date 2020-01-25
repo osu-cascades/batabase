5ce43b6fce0b35a767858b1b8e4f34201d6b84fd
Bryce Graves <brycegraves97@gmail.com>
2020-01-25 01:59:13 -0800

    OrientationSpec: format via rubocop

--- spec/models/orientation_spec.rb
+++ spec/models/orientation_spec.rb
@@ -1,35 +1,36 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe Orientation, type: :model do
   let(:orientation) { FactoryBot.build_stubbed(:orientation, :north) }
 
-  describe "Orientation Validation" do
-    context "Orientation is valid if" do
-      it "It has a named direction" do
-        [ "N", "W", "E", "S", "NW", "NE", "SW", "SE" ].each do |direction|
+  describe 'Orientation Validation' do
+    context 'Orientation is valid if' do
+      it 'It has a named direction' do
+        ['N', 'W', 'E', 'S', 'NW', 'NE', 'SW', 'SE'].each do |direction|
           orientation.direction = direction
           expect(orientation).to be_valid
         end
       end
     end
 
-    context "Orientation is invalid if" do
-      it "It contains no direction" do
-        orientation.direction = ""
+    context 'Orientation is invalid if' do
+      it 'It contains no direction' do
+        orientation.direction = ''
 
         expect(orientation).to_not be_valid
       end
 
       it "It has a direction that isn't a named direction" do
-        orientation.direction = "WN"
+        orientation.direction = 'WN'
 
         expect(orientation).to_not be_valid
 
-        orientation.direction = "Q"
+        orientation.direction = 'Q'
 
         expect(orientation).to_not be_valid
       end
     end
   end
-
 end

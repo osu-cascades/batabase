7fe5b47c3d28914818d2776c341bef7b7a0ad2ba
Bryce Graves <brycegraves97@gmail.com>
2020-01-25 00:28:35 -0800

    LocalHabitat: freeze string literals for better performance

--- app/models/local_habitat.rb
+++ app/models/local_habitat.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class LocalHabitat < ApplicationRecord
   validates :label, presence: true, allow_blank: false, inclusion: {
-    in: [ "mixed conifer", "dry conifer", "hardwood", "alpine forest", "mesic forest", "urban", "agriculture", "grassland", "shrub-steppe"]
+    in: ['mixed conifer', 'dry conifer', 'hardwood', 'alpine forest', 'mesic forest', 'urban', 'agriculture', 'grassland', 'shrub-steppe']
   }
 end

c541da260b526282460c403b23c83760bad33c6d
Bryce Graves <brycegraves97@gmail.com>
2020-01-25 01:28:18 -0800

    TriggerSensitivitySpec: format via rubocop

--- spec/models/trigger_sensitivity_spec.rb
+++ spec/models/trigger_sensitivity_spec.rb
@@ -1,31 +1,32 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe TriggerSensitivity, type: :model do
   let(:trigger_sensitivity) { FactoryBot.build_stubbed(:trigger_sensitivity) }
 
-  describe "Trigger Sensitivity Validation" do
-    context "Trigger sensitivity is valid if" do
-      it "It has a valid sensitivity" do
-        [ "VERY LOW", "LOW", "MED", "HIGH", "VERY HIGH" ].each do |sensitivity|
+  describe 'Trigger Sensitivity Validation' do
+    context 'Trigger sensitivity is valid if' do
+      it 'It has a valid sensitivity' do
+        ['VERY LOW', 'LOW', 'MED', 'HIGH', 'VERY HIGH'].each do |sensitivity|
           trigger_sensitivity.sensitivity = sensitivity
           expect(trigger_sensitivity).to be_valid
         end
       end
     end
 
-    context "Trigger sensitivity is invalid if" do
-      it "It has no sensitivity" do
-        trigger_sensitivity.sensitivity = ""
+    context 'Trigger sensitivity is invalid if' do
+      it 'It has no sensitivity' do
+        trigger_sensitivity.sensitivity = ''
 
         expect(trigger_sensitivity).to_not be_valid
       end
 
       it "It has sensitivity that isn't a provided setting" do
-        trigger_sensitivity.sensitivity = "INVALID SENSITIVITY"
+        trigger_sensitivity.sensitivity = 'INVALID SENSITIVITY'
 
         expect(trigger_sensitivity).to_not be_valid
       end
     end
   end
-
 end

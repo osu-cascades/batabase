5878f9526df21681f0a4dfa701b1f4ae91f30ec8
Bryce Graves <brycegraves97@gmail.com>
2020-01-24 20:39:41 -0800

    Upload: freeze string literals for better performance

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Upload < ApplicationRecord
   validates :data, presence: true, allow_blank: false
-  validates :filename, presence: true, allow_blank: false, format: { with: /.csv\z/i, message: "Only CSV files are allowed" }
-  validates :upload_type, presence: true, allow_blank: false, inclusion: { in: [ "meta", "sono" ] }
+  validates :filename, presence: true, allow_blank: false, format: { with: /.csv\z/i, message: 'Only CSV files are allowed' }
+  validates :upload_type, presence: true, allow_blank: false, inclusion: { in: %w[meta sono] }
 end

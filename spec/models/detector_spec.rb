51835a7d81437ff1df333288b79857c93b03abc7
Bryce Graves <brycegraves97@gmail.com>
2020-01-25 01:29:21 -0800

    DetectorSpec: format via rubocop

--- spec/models/detector_spec.rb
+++ spec/models/detector_spec.rb
@@ -1,56 +1,57 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe Detector, type: :model do
   let(:detector) { FactoryBot.build_stubbed(:detector) }
 
-  describe "Detector Validation" do
-    context "Detector is valid if" do
-      it "It has a manufacturer" do
-        detector.manufacturer = "FAKE MANUFACTURER"
+  describe 'Detector Validation' do
+    context 'Detector is valid if' do
+      it 'It has a manufacturer' do
+        detector.manufacturer = 'FAKE MANUFACTURER'
 
         expect(detector).to be_valid
       end
 
-      it "It has a model" do
-        detector.model = "FAKE MODEL"
+      it 'It has a model' do
+        detector.model = 'FAKE MODEL'
 
         expect(detector).to be_valid
       end
 
-      it "It has a serial number" do
-        detector.serial_number = "FAKE SERIAL NUMBER"
+      it 'It has a serial number' do
+        detector.serial_number = 'FAKE SERIAL NUMBER'
 
         expect(detector).to be_valid
       end
     end
 
-    context "Detector is invalid if" do
-      it "It has no manufacturer" do
-        detector.manufacturer = ""
+    context 'Detector is invalid if' do
+      it 'It has no manufacturer' do
+        detector.manufacturer = ''
 
         expect(detector).to_not be_valid
       end
 
-      it "It has no model" do
-        detector.model = ""
+      it 'It has no model' do
+        detector.model = ''
 
         expect(detector).to_not be_valid
       end
 
-      it "It has no serial number" do
-        detector.serial_number = ""
+      it 'It has no serial number' do
+        detector.serial_number = ''
 
         expect(detector).to_not be_valid
       end
     end
   end
 
-  describe "Detector Functionality" do
-    context "Organization Interactions" do
-      it "It can return what organization owns the detector" do
+  describe 'Detector Functionality' do
+    context 'Organization Interactions' do
+      it 'It can return what organization owns the detector' do
         expect(detector.owner).to eq('FAKE NAME')
       end
     end
   end
-
 end

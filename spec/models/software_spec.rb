cc939f47b76b93da1aa0f5a802f8e7c5dd23a657
Bryce Graves <brycegraves97@gmail.com>
2020-01-25 01:31:51 -0800

    SoftwareSpec: format via rubocop

--- spec/models/software_spec.rb
+++ spec/models/software_spec.rb
@@ -1,38 +1,39 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe Software, type: :model do
   let(:software) { FactoryBot.build_stubbed(:software, :sonobat) }
 
-  describe "Software Validation" do
-    context "Software is valid if" do
-      it "It has a name of Sonobat 4 or Kaleidoscope" do
-        software.name = "Sonobat 4"
+  describe 'Software Validation' do
+    context 'Software is valid if' do
+      it 'It has a name of Sonobat 4 or Kaleidoscope' do
+        software.name = 'Sonobat 4'
 
         expect(software).to be_valid
 
-        software.name = "Kaleidoscope"
+        software.name = 'Kaleidoscope'
 
         expect(software).to be_valid
       end
     end
 
-    context "Software is invalid if" do
-      it "It has no name" do
-        software.name = ""
+    context 'Software is invalid if' do
+      it 'It has no name' do
+        software.name = ''
 
         expect(software).to_not be_valid
       end
 
       it "It has name that isn't Sonobat 4 or Kaleidoscope" do
-        software.name = "Sonobat 3"
+        software.name = 'Sonobat 3'
 
         expect(software).to_not be_valid
 
-        software.name = "Caleidoscope"
+        software.name = 'Caleidoscope'
 
         expect(software).to_not be_valid
       end
     end
   end
-
 end

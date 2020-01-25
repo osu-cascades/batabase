79145dfaa7902a20c37cdc6a8791b90e17ebfe94
Bryce Graves <brycegraves97@gmail.com>
2020-01-25 01:28:58 -0800

    DetectionTargetSpec: format via rubocop

--- spec/models/detection_target_spec.rb
+++ spec/models/detection_target_spec.rb
@@ -1,24 +1,25 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe DetectionTarget, type: :model do
   let(:detection_target) { FactoryBot.build_stubbed(:detection_target) }
 
-  describe "Detection Target Validation" do
-    context "An detection target is valid if" do
-      it "It has a label" do
-        detection_target.label = "FAKE LABEL"
+  describe 'Detection Target Validation' do
+    context 'An detection target is valid if' do
+      it 'It has a label' do
+        detection_target.label = 'FAKE LABEL'
 
         expect(detection_target).to be_valid
       end
     end
 
-    context "An detection target is invalid if" do
-      it "It has no label" do
-        detection_target.label = ""
+    context 'An detection target is invalid if' do
+      it 'It has no label' do
+        detection_target.label = ''
 
         expect(detection_target).to_not be_valid
       end
     end
   end
-
 end

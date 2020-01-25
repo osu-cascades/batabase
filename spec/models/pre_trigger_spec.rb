20dfc1f84c6086fd9a27aaabc34489141c9c5d2c
Bryce Graves <brycegraves97@gmail.com>
2020-01-25 01:30:44 -0800

    PreTriggerSpec: format via rubocop

--- spec/models/pre_trigger_spec.rb
+++ spec/models/pre_trigger_spec.rb
@@ -1,10 +1,12 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe PreTrigger, type: :model do
   let(:pre_trigger) { FactoryBot.build_stubbed(:pre_trigger) }
 
-  describe "Pre Trigger Validation" do
-    context "A pre trigger is valid if" do
+  describe 'Pre Trigger Validation' do
+    context 'A pre trigger is valid if' do
       it "It has a delay that's a float" do
         pre_trigger.delay = 4.2
 
@@ -12,17 +14,16 @@ RSpec.describe PreTrigger, type: :model do
       end
     end
 
-    context "A pre trigger is invalid if" do
+    context 'A pre trigger is invalid if' do
       it "It has a delay that isn't a float" do
         pre_trigger.delay = nil
 
         expect(pre_trigger).to_not be_valid
 
-        pre_trigger.delay = "INVALID"
+        pre_trigger.delay = 'INVALID'
 
         expect(pre_trigger).to_not be_valid
       end
     end
   end
-
 end

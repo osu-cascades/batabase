7a9bb0fb31ea3d6a81dd4354bf75d746949a336b
Bryce Graves <brycegraves97@gmail.com>
2020-01-25 01:31:35 -0800

    SamplingFrequencySpec: format via rubocop

--- spec/models/sampling_frequency_spec.rb
+++ spec/models/sampling_frequency_spec.rb
@@ -1,10 +1,12 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe SamplingFrequency, type: :model do
   let(:sampling_frequency) { FactoryBot.build_stubbed(:sampling_frequency) }
 
-  describe "Sampling Frequency Validation" do
-    context "A sampling frequency is valid if" do
+  describe 'Sampling Frequency Validation' do
+    context 'A sampling frequency is valid if' do
       it "It has a frequency that's a float" do
         sampling_frequency.frequency = 4.2
 
@@ -12,23 +14,22 @@ RSpec.describe SamplingFrequency, type: :model do
       end
     end
 
-    context "A sampling frequency is invalid if" do
+    context 'A sampling frequency is invalid if' do
       it "It has a frequency that isn't a float" do
         sampling_frequency.frequency = nil
 
         expect(sampling_frequency).to_not be_valid
 
-        sampling_frequency.frequency = "INVALID"
+        sampling_frequency.frequency = 'INVALID'
 
         expect(sampling_frequency).to_not be_valid
       end
 
-      it "It has a frequency of zero" do
+      it 'It has a frequency of zero' do
         sampling_frequency.frequency = 0
 
         expect(sampling_frequency).to_not be_valid
       end
     end
   end
-
 end

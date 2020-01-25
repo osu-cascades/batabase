45c51234ba540376816575529315c8915ad658d6
Bryce Graves <brycegraves97@gmail.com>
2020-01-25 01:32:46 -0800

    UploadSpec: format via rubocop

--- spec/system/upload_spec.rb
+++ spec/system/upload_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 GENERATED_UPLOAD_COUNT = 3
@@ -13,38 +15,38 @@ RSpec.describe 'Upload Flows', type: :system do
       1.upto(GENERATED_UPLOAD_COUNT) { |i| expect(page).to have_content("FAKE_FILENAME_#{i}.csv") }
     end
 
-    it 'A user can click to view the data of an upload', js:true do
+    it 'A user can click to view the data of an upload', js: true do
       first('a', text: 'View Upload').click
 
-      expect(page).to have_content("FAKE_FILENAME_1.csv")
+      expect(page).to have_content('FAKE_FILENAME_1.csv')
     end
 
-    it 'A user can click to delete an upload', js:true do
-      expect(page).to have_content("FAKE_FILENAME_1.csv")
+    it 'A user can click to delete an upload', js: true do
+      expect(page).to have_content('FAKE_FILENAME_1.csv')
 
       first('form').click
 
-      expect(page).to_not have_content("FAKE_FILENAME_1.csv")
+      expect(page).to_not have_content('FAKE_FILENAME_1.csv')
     end
 
-    it 'A user can click to navigate to new upload page', js:true do
+    it 'A user can click to navigate to new upload page', js: true do
       find('a', text: 'Add Upload').click
 
-      expect(page).to_not have_content("FAKE_FILENAME_1.csv")
+      expect(page).to_not have_content('FAKE_FILENAME_1.csv')
     end
   end
 
-  context "New" do
+  context 'New' do
     before :each do
       visit new_upload_path
     end
-    
+
     it 'A user successfully uploads a csv file' do
       select('Site Metadata', from: 'Upload Type')
       attach_file('Data', 'spec/fixtures/meta_upload.csv')
       click_button 'Upload CSV'
 
-      expect(page).to have_content("Commit Upload")
+      expect(page).to have_content('Commit Upload')
     end
 
     it 'A user unsuccessfully attempts to upload a csv file' do
@@ -52,20 +54,20 @@ RSpec.describe 'Upload Flows', type: :system do
       attach_file('Data', 'spec/fixtures/empty_upload.csv')
       click_button 'Upload CSV'
 
-      expect(page).to have_content("File must contain data")
+      expect(page).to have_content('File must contain data')
     end
 
     # TODO: fix the form and how it's validation works
     # TODO: add test to verify file name is reflected and add that to how the form works to make it pass
     # TODO: change this test to check for the error message for a miss named file
     it "A user unsuccessfully attempts to upload a file that isn't a csv" do
-      pending("See TODOs")
+      pending('See TODOs')
 
       select('Site Metadata', from: 'Upload type')
       attach_file('Data', 'spec/fixtures/bad_upload.sbeav')
       click_button 'Upload CSV'
 
-      expect(page).to have_content("Only CSV files are allowed")
+      expect(page).to have_content('Only CSV files are allowed')
     end
   end
 end

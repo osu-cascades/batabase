0c9965c6eb66853f09472ba54bb80702bd64b85b
Bryce Graves <brycegraves97@gmail.com>
2020-01-25 00:35:16 -0800

    TriggerSensitivity: freeze string literals for better performance

--- app/models/trigger_sensitivity.rb
+++ app/models/trigger_sensitivity.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class TriggerSensitivity < ApplicationRecord
-  validates :sensitivity, presence: true, allow_blank: false, inclusion: { in: [ "VERY LOW", "LOW", "MED", "HIGH", "VERY HIGH" ] }
+  validates :sensitivity, presence: true, allow_blank: false, inclusion: { in: ['VERY LOW', 'LOW', 'MED', 'HIGH', 'VERY HIGH'] }
 end

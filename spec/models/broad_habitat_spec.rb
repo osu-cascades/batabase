82a4c469f1668b03dca51b446e739fd0f3249426
Bryce Graves <brycegraves97@gmail.com>
2020-01-25 01:26:17 -0800

    BroadHabitatSpec: format via rubocop

--- spec/models/broad_habitat_spec.rb
+++ spec/models/broad_habitat_spec.rb
@@ -1,24 +1,25 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe BroadHabitat, type: :model do
   let(:broad_habitat) { FactoryBot.build_stubbed(:broad_habitat) }
 
-  describe "Broad Habitat Validation" do
-    context "An broad habitat is valid if" do
-      it "It has a habitat name" do
-        broad_habitat.name = "FAKE NAME"
+  describe 'Broad Habitat Validation' do
+    context 'An broad habitat is valid if' do
+      it 'It has a habitat name' do
+        broad_habitat.name = 'FAKE NAME'
 
         expect(broad_habitat).to be_valid
       end
     end
 
-    context "An broad habitat is invalid if" do
-      it "It has no habitat name" do
-        broad_habitat.name = ""
+    context 'An broad habitat is invalid if' do
+      it 'It has no habitat name' do
+        broad_habitat.name = ''
 
         expect(broad_habitat).to_not be_valid
       end
     end
   end
-
 end

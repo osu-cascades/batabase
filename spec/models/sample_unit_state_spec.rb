f5f2a34f8ceece0fad6346a13de27d5b658cea3a
Bryce Graves <brycegraves97@gmail.com>
2020-01-25 01:31:16 -0800

    SampleUnitStateSpec: format via rubocop

--- spec/models/sample_unit_state_spec.rb
+++ spec/models/sample_unit_state_spec.rb
@@ -1,10 +1,12 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe SampleUnitState, type: :model do
   let(:sample_unit_state) { FactoryBot.build_stubbed(:sample_unit_state) }
 
-  describe "Sample Unit State Validation" do
-    context "An sample unit state is valid if" do
+  describe 'Sample Unit State Validation' do
+    context 'An sample unit state is valid if' do
       it "It has a percentage that's a float" do
         sample_unit_state.percentage = 42.42
 
@@ -12,18 +14,18 @@ RSpec.describe SampleUnitState, type: :model do
       end
     end
 
-    context "An sample unit state is invalid if" do
+    context 'An sample unit state is invalid if' do
       it "It has a percentage that isn't a float" do
         sample_unit_state.percentage = nil
 
         expect(sample_unit_state).to_not be_valid
 
-        sample_unit_state.percentage = "INVALID"
+        sample_unit_state.percentage = 'INVALID'
 
         expect(sample_unit_state).to_not be_valid
       end
 
-      it "It has a percentage of zero" do
+      it 'It has a percentage of zero' do
         sample_unit_state.percentage = 0
 
         expect(sample_unit_state).to_not be_valid
@@ -31,26 +33,25 @@ RSpec.describe SampleUnitState, type: :model do
     end
   end
 
-  describe "Sample Unit State Functionality" do
-    context "State Interactions" do
+  describe 'Sample Unit State Functionality' do
+    context 'State Interactions' do
       it 'A sample unit state can return what state it belongs to' do
         expect(sample_unit_state.state.class).to be(State)
       end
 
       it 'A sample unit state can return the name of the state it belongs to' do
-        expect(sample_unit_state.name).to eq("FAKE NAME")
+        expect(sample_unit_state.name).to eq('FAKE NAME')
       end
 
       it 'A sample unit state can return the abbreviation of the state it belongs to' do
-        expect(sample_unit_state.abbreviation).to eq("FAKE ABBREVIATION")
+        expect(sample_unit_state.abbreviation).to eq('FAKE ABBREVIATION')
       end
     end
 
-    context "Sample Unit Interactions" do
+    context 'Sample Unit Interactions' do
       it 'A sample unit state can return what sample unit it belongs to' do
         expect(sample_unit_state.sample_unit.class).to be(SampleUnit)
       end
     end
   end
-
 end

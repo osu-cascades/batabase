b95e9d99e098bc4e6a8bdd73da08e930a0a3a7c5
Mackenzie Hatfield <therealbakoonzar@gmail.com>
2020-02-04 15:32:27 -0800

    DetectorLocation: add basic validations.

--- app/models/detector_location.rb
+++ app/models/detector_location.rb
@@ -1,3 +1,8 @@
 class DetectorLocation < ApplicationRecord
   belongs_to :sample_unit
+  validates :quad_id, presence: true, allow_blank: false, inclusion: {
+    in: ['NE', 'NW', 'SE', 'SW']
+  }
+  validates :quad_no, presence: true, inclusion: 1..10
+  
 end

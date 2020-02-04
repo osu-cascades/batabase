629aa357c689097226f97ae2b5ceb73a4c455199
Bryce Graves <brycegraves97@gmail.com>
2020-02-03 22:00:09 -0800

    CreateSoftware: add missing columns

--- db/migrate/20191122182446_create_software.rb
+++ db/migrate/20191122182446_create_software.rb
@@ -4,6 +4,13 @@ class CreateSoftware < ActiveRecord::Migration[6.0]
   def change
     create_table :softwares do |t|
       t.string :name
+      t.string :version
+      t.string :classifier_package
+      t.float :acceptable_call_quality
+      t.float :sqnc_decision_threshold
+      t.integer :max_no_calls
+      t.string :log_file_notes
+      t.string :other_settings
     end
   end
 end

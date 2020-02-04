8cb2af608d554baa3b7d9ed1d5186cddabd093ac
Bryce Graves <brycegraves97@gmail.com>
2020-02-03 22:00:36 -0800

    Software: stub out software validations

--- app/models/software.rb
+++ app/models/software.rb
@@ -2,4 +2,11 @@
 
 class Software < ApplicationRecord
   validates :name, presence: true, allow_blank: false, inclusion: { in: ['Sonobat 4', 'Kaleidoscope'] }
+  validates :version
+  validates :classifier_package
+  validates :acceptable_call_quality
+  validates :sequence_decision_threshold
+  validates :max_no_calls
+  validates :log_file_notes
+  validates :other_settings
 end

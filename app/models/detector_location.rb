773b832000e896cc2c5ebbb13bae095cc3c0dfaa
Bryce Graves <brycegraves97@gmail.com>
2020-02-11 13:38:27 -0800

    DetectorLocation: add validation for only whole number values

--- app/models/detector_location.rb
+++ app/models/detector_location.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class DetectorLocation < ApplicationRecord
   belongs_to :sample_unit
   belongs_to :detection_target
@@ -5,12 +7,12 @@ class DetectorLocation < ApplicationRecord
   belongs_to :local_habitat
 
   validates :quad_id, presence: true, allow_blank: false, inclusion: {
-    in: ['NE', 'NW', 'SE', 'SW']
+    in: ['NW', 'NE', 'SW' 'SE']
   }
 
-  validates :quad_no, presence: true, inclusion: 1..10
+  validates :quad_no, presence: true, numericality: { only_integer: true }, inclusion: 1..20
 
   after_create do
-    self.location_id = "#{self.sample_unit_id}_#{self.quad_id}#{self.quad_no}"
+    self.location_id = "#{sample_unit_id}_#{quad_id}#{quad_no}"
   end
 end

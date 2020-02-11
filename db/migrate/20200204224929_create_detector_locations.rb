c0fb542cf6d22fbb81dd9a3ea5b3059214122c3b
Bryce Graves <brycegraves97@gmail.com>
2020-02-11 13:40:45 -0800

    Migrations: remove key columns during detector locations creation and add land ownership column

--- db/migrate/20200204224929_create_detector_locations.rb
+++ db/migrate/20200204224929_create_detector_locations.rb
@@ -1,20 +1,17 @@
+# frozen_string_literal: true
+
 class CreateDetectorLocations < ActiveRecord::Migration[6.0]
   def change
     create_table :detector_locations do |t|
-      t.bigint :sample_unit_id
       t.string :quad_id
       t.integer :quad_no
-      t.string :location_id
+      t.string :location_identifier
       t.string :location_name
       t.float :latitude
       t.float :longitude
       t.float :elevation
       t.string :driving_directions
-      t.bigint :local_habitat_id
-      t.bigint :detection_target_id
-      t.bigint :target_descriptor_id
-
-      t.timestamps
+      t.string :land_ownership
     end
   end
 end

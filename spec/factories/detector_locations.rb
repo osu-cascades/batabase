9f0ee87d0af20bee41b78b69e6fbb3889be16f66
Bryce Graves <brycegraves97@gmail.com>
2020-02-11 13:41:20 -0800

    DetectorLocations: add relation creation and update names to match other factory convention

--- spec/factories/detector_locations.rb
+++ spec/factories/detector_locations.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :detector_location do
-    sample_unit_id { "" }
-    quad_id { "MyString" }
+    sample_unit
+    local_habitat
+    target_descriptor
+
+    quad_id { 'NW' }
     quad_no { 1 }
-    location_id { "MyString" }
-    location_name { "MyString" }
-    latitude { 1.5 }
-    longitude { 1.5 }
-    elevation { 1.5 }
-    driving_directions { "MyString" }
-    local_habitat_id { "" }
-    detection_target_id { "" }
-    target_descriptor_id { "" }
+    location_identifier { "#{sample_unit.id}_#{quad_id}#{quad_no}" }
+    location_name { 'FAKE NAME' }
+    latitude { 42.42 }
+    longitude { 42.42 }
+    elevation { 42.42 }
+    driving_directions { 'FAKE DRIVING DIRECTIONS' }
   end
 end

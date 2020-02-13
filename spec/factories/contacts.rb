70ea4451e85ddb67590738f9ec03c3e316a047d2
Bryce Graves <brycegraves97@gmail.com>
2020-02-12 17:24:40 -0800

    Contacts: add trait for related software and change names to reflect previous convention

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -10,5 +10,18 @@ FactoryBot.define do
     trait :has_organization do
       organization
     end
+
+    trait :has_software do
+      softwares do
+        Array.new(5) do |i|
+          Software.new(
+            version: "FAKE VERSION #{i.humanize.upcase}",
+            classifier_package: 'FAKE CLASSIFIER PACKAGE',
+            log_file_notes: 'FAKE LOGFILE NOTES',
+            other_settings: 'FAKE OTHER SETTINGS'
+          )
+        end
+      end
+    end
   end
 end

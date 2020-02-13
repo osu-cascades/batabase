fcd0795e4f45659dbb10872bcd4a3d936d55c3ea
Bryce Graves <brycegraves97@gmail.com>
2020-02-12 17:23:30 -0800

    CreateSoftware: set default value for software name

--- db/migrate/20191122182446_create_software.rb
+++ db/migrate/20191122182446_create_software.rb
@@ -3,7 +3,7 @@
 class CreateSoftware < ActiveRecord::Migration[6.0]
   def change
     create_table :softwares do |t|
-      t.string :name
+      t.string :name, default: 'Sonobat 4'
       t.string :version
       t.string :classifier_package
       t.float :acceptable_call_quality, default: 0.8

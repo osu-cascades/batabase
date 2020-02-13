fa0f4fe61b72ed59541e608513359116cbe1c675
Bryce Graves <brycegraves97@gmail.com>
2020-02-12 17:33:14 -0800

    DetectorLocations: add detection target for object relation and add location identifier

--- spec/factories/detector_locations.rb
+++ spec/factories/detector_locations.rb
@@ -4,11 +4,12 @@ FactoryBot.define do
   factory :detector_location do
     sample_unit
     local_habitat
+    detection_target
     target_descriptor
 
     quad_id { 'NW' }
     quad_no { 1 }
-    location_identifier { "#{sample_unit.id}_#{quad_id}#{quad_no}" }
+    location_identifier { 'FAKE LOCATION IDENTIFIER' }
     location_name { 'FAKE NAME' }
     latitude { 42.42 }
     longitude { 42.42 }

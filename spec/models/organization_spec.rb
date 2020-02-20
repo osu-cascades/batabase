761471842c240e153894c4d0d182f2472b86970f
Bryce Graves <brycegraves97@gmail.com>
2020-02-19 16:22:48 -0800

    OrganizationSpec: add tests for detector and contact associations

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -4,6 +4,8 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
   let(:organization) { FactoryBot.build_stubbed(:organization) }
+  let(:organization_with_detectors) { FactoryBot.build_stubbed(:organization, :has_detectors) }
+  let(:organization_with_contacts) { FactoryBot.build_stubbed(:organization, :has_contacts) }
 
   describe 'Organization Validation' do
     context 'Organization is valid if' do
@@ -25,6 +27,28 @@ RSpec.describe Organization, type: :model do
         organization.name = ''
 
         expect(organization).to_not be_valid
+      end
+    end
+  end
+
+  describe 'Organization Functionality' do
+    context 'Detector Interactions' do
+      it 'A organization can fetch all of the detectors associated with it' do
+        organization_with_detectors.detectors.each do |detector|
+          expect(detector.class).to be(Detector)
+        end
+
+        expect(organization_with_detectors.detectors.length).to eq(5)
+      end
+    end
+
+    context 'Contact Interactions' do
+      it 'A organization can fetch all of the contacts associated with it' do
+        organization_with_contacts.contacts.each do |contact|
+          expect(contact.class).to be(Contact)
+        end
+
+        expect(organization_with_contacts.contacts.length).to eq(5)
       end
     end
   end

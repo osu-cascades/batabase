a7230cdca96759ec3c95713ce0ba880978f55258
Bryce Graves <brycegraves97@gmail.com>
2020-02-19 16:20:17 -0800

    DetectionTargetSpec: add tests for target descriptor and detector location associations

--- spec/models/detection_target_spec.rb
+++ spec/models/detection_target_spec.rb
@@ -4,6 +4,8 @@ require 'rails_helper'
 
 RSpec.describe DetectionTarget, type: :model do
   let(:detection_target) { FactoryBot.build_stubbed(:detection_target) }
+  let(:detection_target_with_target_descriptors) { FactoryBot.build_stubbed(:detection_target, :has_target_descriptors) }
+  let(:detection_target_with_detector_locations) { FactoryBot.build_stubbed(:detection_target, :has_detector_locations) }
 
   describe 'Detection Target Validation' do
     context 'An detection target is valid if' do
@@ -19,6 +21,28 @@ RSpec.describe DetectionTarget, type: :model do
         detection_target.label = ''
 
         expect(detection_target).to_not be_valid
+      end
+    end
+  end
+
+  describe 'Detection Target Functionality' do
+    context 'Target Descriptor Interactions' do
+      it 'A detection target can fetch all of the target descriptors associated with it' do
+        detection_target_with_target_descriptors.target_descriptors.each do |target_descriptor|
+          expect(target_descriptor.class).to be(TargetDescriptor)
+        end
+
+        expect(detection_target_with_target_descriptors.target_descriptors.length).to eq(5)
+      end
+    end
+
+    context 'Detector Locations Interactions' do
+      it 'A detection target can fetch all of the detector locations associated with it' do
+        detection_target_with_detector_locations.detector_locations.each do |detector_location|
+          expect(detector_location.class).to be(DetectorLocation)
+        end
+
+        expect(detection_target_with_detector_locations.detector_locations.length).to eq(5)
       end
     end
   end

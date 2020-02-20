3117adfe1beecd8194c86432b16ade62d109e43f
Bryce Graves <brycegraves97@gmail.com>
2020-02-19 16:03:48 -0800

    BroadHabitats: add traits for building broad habitat forms and sample units

--- spec/factories/broad_habitats.rb
+++ spec/factories/broad_habitats.rb
@@ -3,5 +3,13 @@
 FactoryBot.define do
   factory :broad_habitat do
     name { 'FAKE NAME' }
+
+    trait :has_broad_habitat_forms do
+      broad_habitat_forms { FactoryBot.build_list(:broad_habitat_form, 5) }
+    end
+
+    trait :has_sample_units do
+      sample_units { FactoryBot.build_list(:sample_unit, 5) }
+    end
   end
 end

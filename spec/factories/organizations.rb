b84192558dbebeb54320dbaa759e04f1132b59b7
Bryce Graves <brycegraves97@gmail.com>
2020-02-19 16:08:44 -0800

    Organizations: add trait for building detectors and contacts

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -3,5 +3,13 @@
 FactoryBot.define do
   factory :organization do
     name { 'FAKE NAME' }
+
+    trait :has_detectors do
+      detectors { FactoryBot.build_list(:detector, 5) }
+    end
+
+    trait :has_contacts do
+      contacts { FactoryBot.build_list(:contact, 5) }
+    end
   end
 end

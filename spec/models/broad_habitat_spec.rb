5c57f5232adbe14ec81b795ec5568d6026e0df40
Bryce Graves <brycegraves97@gmail.com>
2020-02-19 16:16:26 -0800

    BroadHabitatSpec: add tests for broad habitat form and sample unit associations

--- spec/models/broad_habitat_spec.rb
+++ spec/models/broad_habitat_spec.rb
@@ -4,6 +4,8 @@ require 'rails_helper'
 
 RSpec.describe BroadHabitat, type: :model do
   let(:broad_habitat) { FactoryBot.build_stubbed(:broad_habitat) }
+  let(:broad_habitat_with_broad_habitat_forms) { FactoryBot.build_stubbed(:broad_habitat, :has_broad_habitat_forms) }
+  let(:broad_habitat_with_sample_units) { FactoryBot.build_stubbed(:broad_habitat, :has_sample_units) }
 
   describe 'Broad Habitat Validation' do
     context 'An broad habitat is valid if' do
@@ -19,6 +21,28 @@ RSpec.describe BroadHabitat, type: :model do
         broad_habitat.name = ''
 
         expect(broad_habitat).to_not be_valid
+      end
+    end
+  end
+
+  describe 'Broad Habitat Functionality' do
+    context 'Broad Habitat Form Interactions' do
+      it 'A broad habitat can fetch the broad habitat forms associated with it' do
+        broad_habitat_with_broad_habitat_forms.broad_habitat_forms.each do |broad_habitat_form|
+          expect(broad_habitat_form.class).to be(BroadHabitatForm)
+        end
+
+        expect(broad_habitat_with_broad_habitat_forms.broad_habitat_forms.length).to eq(5)
+      end
+    end
+
+    context 'Sample Unit Interactions' do
+      it 'A broad habitat can fetch the sample units associated with' do
+        broad_habitat_with_sample_units.sample_units.each do |sample_unit|
+          expect(sample_unit.class).to be(SampleUnit)
+        end
+
+        expect(broad_habitat_with_sample_units.sample_units.length).to eq(5)
       end
     end
   end

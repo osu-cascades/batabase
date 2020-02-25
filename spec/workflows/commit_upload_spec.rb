9b44fe29c4d8d8cd49859d40028d630df691e295
Bryce Graves <brycegraves97@gmail.com>
2020-02-25 15:21:39 -0800

    CommitUploadSpec: add test for accessing data in workflow

--- spec/workflows/commit_upload_spec.rb
+++ spec/workflows/commit_upload_spec.rb
@@ -3,9 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe CommitUpload, type: :workflow do
-  it 'exists' do
+  it 'Exists' do
     commit_upload = CommitUpload.new
 
     expect(commit_upload).to be_a CommitUpload
+  end
+
+  it 'Can access data given to it' do
+    expected = 'FAKE DATA'
+    commit_upload = CommitUpload.new(expected)
+
+    expect(commit_upload.data).to eq(expected)
   end
 end

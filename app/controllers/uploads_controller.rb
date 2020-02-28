e0145ee2e2bf95b214779935b395b3003e40f0fa
Bryce Graves <brycegraves97@gmail.com>
2020-02-28 13:54:40 -0800

    CommitUpload: add creating detector from upload, tests for that, and adding it on upload

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -47,9 +47,13 @@ class UploadsController < ApplicationController
   end
 
   def commit
-    @upload = Upload.find(params[:upload_id])
+    upload = Upload.find(params[:upload_id])
     # This will be where the work happens for turning
     # the CSV into tables in db
+
+    workflow = CommitUpload.new(CSV.parse(upload.data, headers: true))
+    workflow.run
+
     redirect_to uploads_path
   end
 end
--- spec/workflows/commit_upload_spec.rb
+++ spec/workflows/commit_upload_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'csv'
 require 'rails_helper'
 
 RSpec.describe CommitUpload, type: :workflow do
@@ -14,5 +15,20 @@ RSpec.describe CommitUpload, type: :workflow do
     commit_upload = CommitUpload.new(expected)
 
     expect(commit_upload.data).to eq(expected)
+  end
+
+  it 'Can accept csv data' do
+    expected = CSV.read(Rails.root.join('spec/fixtures/raw_oregon_survey123_output.csv'))
+
+    commit_upload = CommitUpload.new(expected)
+
+    expect(commit_upload.data).to eq(expected)
+  end
+
+  it 'Can create a detector from raw survey 123 data' do
+    expected = CSV.read(Rails.root.join('spec/fixtures/populated_bulk_upload_template.csv'), headers: true)
+    create(:organization, name: 'OSU')
+
+    CommitUpload.new(expected).run
   end
 end

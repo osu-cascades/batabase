4fd9d73316a18761d3d0ef9475a35ce65514c7be
MackHatfield <therealbakoonzar@gmail.com>
2020-04-15 18:12:29 -0700

    Disable confirmation for now.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable, :lockable, :trackable
+  
+  protected
+  def confirmation_required?
+    false
+  end
 end

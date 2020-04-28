01749d7c9ebe5f4e83b742625a56ab03397f9db1
MackHatfield <therealbakoonzar@gmail.com>
2020-04-28 14:04:19 -0700

    Add deployments system spec.

---
+++ spec/system/deployments_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Deployments Flow', type: :system do
+  let(:user) { FactoryBot.create(:user) }
+
+  context 'Deployments' do
+    before :each do
+      sign_in user
+    end
+
+    it 'A user can visit the page to view all the deployments' do
+      visit deployments_path
+      expect(page).to have_css('table.deployments_grid')
+    end
+
+  end
+end

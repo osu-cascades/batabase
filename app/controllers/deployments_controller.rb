945b41c6bdb445f05fc52fa47c27b01db632f116
MackHatfield <therealbakoonzar@gmail.com>
2020-04-28 17:43:52 -0700

    Add params from form to deployment create action.

--- app/controllers/deployments_controller.rb
+++ app/controllers/deployments_controller.rb
@@ -24,7 +24,7 @@ class DeploymentsController < ApplicationController
     @fields = [
       { type: :text_area, name: :notes, options: {} },
       { type: :text_field, name: :microphone_height_off_ground, options: {} },
-      { type: :text_field, name: :microphone_orientation, options: {} },
+      { type: :select, name: :microphone_orientation, options: { UNKNOWN: '', N: 'N', W: 'W', E: 'E', S: 'S', NW: 'NW', NE: 'NE', SW: 'SW', SE: 'SE', Vertical: 'Vertical' } },
       { type: :text_field, name: :sampling_frequency, options: {} },
       { type: :select, name: :pre_trigger, options: { OFF: "OFF", ON: "ON" } },
       { type: :text_field, name: :recording_length, options: {} },
@@ -46,7 +46,7 @@ class DeploymentsController < ApplicationController
       { type: :datetime_field, name: :deployment_date, options: {} },
       { type: :datetime_field, name: :recovery_date, options: {} },
       { type: :datetime_field, name: :recording_start, options: {} },
-      { type: :datetime_field, name: :recording_start, options: {} },
+      { type: :datetime_field, name: :recording_stop, options: {} },
       { type: :select, name: :primary_contact, options: contact_names },
       { type: :select, name: :recovery_contact, options: contact_names },
       { type: :text_field, name: "Location ID", options: {} },
@@ -54,7 +54,6 @@ class DeploymentsController < ApplicationController
       { type: :text_field, name: "Longitude", options: {} },
       { type: :text_field, name: "Site Name", options: {} },
       { type: :text_area, name: "Directions to Site", options: {} },
-      { type: :text_field, name: "Site Name", options: {} },
       { type: :select, name: "Land Ownership", options: organization_names },
       { type: :select, name: "Detector Serial Number", options: detector_serial_numbers },
       { type: :select, name: "Distance to Clutter", options: distance_ranges },
@@ -67,5 +66,51 @@ class DeploymentsController < ApplicationController
     ] 
 
     @header_text = "Create Deployment"
+  end
+
+  def create
+    notes = params[:deployment][:notes]
+    microphone_height_off_ground = params[:deployment][:microphone_height_off_ground]
+    microphone_orientation = params[:deployment][:microphone_orientation]
+    sampling_frequency = params[:deployment][:sampling_frequency]
+    pre_trigger = params[:deployment][:pre_trigger]
+    recording_length = params[:deployment][:recording_length]
+    hp_filter = params[:deployment][:hp_filter]
+    auto_record = params[:deployment][:auto_record]
+    trigger_sensitivity = params[:deployment][:trigger_sensitivity]
+    input_gain = params[:deployment][:input_gain]
+    trigger_level = params[:deployment][:trigger_level]
+    interval = params[:deployment][:interval]
+    gain = params[:deployment][:gain]
+    sixteen_thousand_high_filter = params[:deployment][:sixteen_thousand_high_filter]
+    sample_rate = params[:deployment][:sample_rate]
+    min_duration = params[:deployment][:min_duration]
+    max_duration = params[:deployment][:max_duration]
+    min_trigger_frequency = params[:deployment][:min_trigger_frequency]
+    trigger_window = params[:deployment][:min_trigger_frequency]
+    max_length = params[:deployment][:max_length]
+    compression = params[:deployment][:compression]
+    deployment_date_string = params[:deployment][:deployment_date]
+    recovery_date_string = params[:deployment][:recovery_date]
+    recording_start_string = params[:deployment][:recording_start]
+    recording_stop_string = params[:deployment][:recording_stop]
+    primary_contact_id = params[:deployment][:primary_contact]
+    recovery_contact_id = params[:deployment][:recovery_contact]
+    location_identifier = params[:deployment][:location_id]
+    latitude = params[:deployment][:latitude]
+    longitude = params[:deployment][:longitude]
+    site_name = params[:deployment][:site_name]
+    direction_to_site = params[:deployment][:directions_to_site]
+    land_ownership = params[:deployment][:land_ownership]
+    detector_serial_number = params[:deployment][:detector_serial_number]
+    distanct_to_clutter = params[:deployment][:distanct_to_clutter]
+    clutter_category = params[:deployment][:clutter_category]
+    clutter_type = params[:deployment][:clutter_type]
+    habitat = params[:deployment][:habitat]
+    target = params[:deployment][:target]
+    descriptor = params[:deployment][:descriptor]
+    comments = params[:deployment][:comments]
+
+    byebug
   end
 end
--- spec/system/deployments_spec.rb
+++ spec/system/deployments_spec.rb
@@ -18,8 +18,66 @@ RSpec.describe 'Deployments Flow', type: :system do
     it 'A user can visit the page to add new deployments' do
       visit deployments_path
       find('a', text: 'Add Deployment').click
-      
+
       expect(page).to have_content('Create Deployment')
+    end
+
+    it 'A user can add a new deployment' do
+      fake_organization = create(:organization, name: 'OSU')
+      create(:contact, first_name: 'FAKE FIRST', last_name: 'FAKE LAST', organization_id: fake_organization.id)
+      create(:detector, serial_number: "12345")
+      create(:distance_range, label: "5m")
+      create(:clutter_percent, label: "10%")
+      create(:clutter_type, name: "FAKE TYPE")
+      create(:local_habitat, label: "mixed conifer")
+      create(:detection_target, label: "FAKE TARGET")
+      create(:target_descriptor, label: "FAKE DESCRIPTOR")
+      create(:sample_unit)
+
+      visit deployments_path
+      find('a', text: 'Add Deployment').click
+
+      fill_in 'Notes', with: 'FAKE NOTES......'
+      fill_in 'Microphone height off ground', with: '9999'
+      select 'N', from: 'Microphone orientation'
+      fill_in 'Sampling frequency', with: '123'
+      select 'OFF', from: 'Pre trigger'
+      fill_in 'Recording length', with: 'VERY LONG'
+      select 'NO', from: 'Hp filter'
+      select 'YES', from: 'Auto record'
+      select 'MED', from: 'Trigger sensitivity'
+      fill_in 'Input gain', with: 'FAKE INPUT GAIN'
+      fill_in 'Trigger level', with: '999999'
+      fill_in 'Interval', with: 'FAKE INTERVAL'
+      fill_in 'Gain', with: 'FAKE GAIN'
+      select 'ON', from: 'Sixteen thousand high filter'
+      fill_in 'Sample rate', with: '99999'
+      fill_in 'Min duration', with: '9999'
+      fill_in 'Max duration', with: 'none'
+      fill_in 'Min trigger frequency', with: '99999'
+      fill_in 'Trigger window', with: '99999'
+      fill_in 'Max length', with: '99999'
+      fill_in 'Compression', with: 'A little bit'
+      #skipping the datepickers for now
+      select 'FAKE LAST, FAKE FIRST', from: 'Primary contact'
+      select 'FAKE LAST, FAKE FIRST', from: 'Recovery contact'
+      fill_in 'Location id', with: '12345_NE1'
+      fill_in 'Latitude', with: 'FAKE LATITUDE'
+      fill_in 'Longitude', with: 'FAKE LONGITUDE'
+      fill_in 'Site name', with: 'FAKE SITE NAME'
+      fill_in 'Directions to site', with: 'FAKE DIRECTIONS'
+      select 'OSU', from: 'Land ownership'
+      select '12345', from: 'Detector serial number'
+      select '5m', from: 'Distance to clutter'
+      select '10%', from: 'Clutter category'
+      select 'FAKE TYPE', from: 'Clutter type'
+      select 'mixed conifer', from: 'Habitat'
+      select 'FAKE DESCRIPTOR', from: 'Descriptor'
+      fill_in 'Comments', with: 'SOME FAKE COMMENTS'
+
+      click_button 'Create Deployment'
+      expect(page).to have_css('table.deployments_grid')
+      expect(page).to have_content('SOME FAKE COMMENTS')
     end
 
   end

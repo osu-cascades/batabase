ff2acf9cf8dfaf20901b6c5db7999dc27debb2e9
Bryce Graves <brycegraves97@gmail.com>
2020-04-30 20:00:03 -0700

    CreateUpdateFormComponent: clean up extra whitespace and format

--- app/components/create_update_form_component.rb
+++ app/components/create_update_form_component.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class CreateUpdateFormComponent < ApplicationComponent
   def initialize(model, fields, header_text)
     @model = model
     @fields = fields
     @header_text = header_text
-  end  
+  end
 end

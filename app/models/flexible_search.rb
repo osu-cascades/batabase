07dd18794d669ac3d2307b95588d743e9a0352d2
Shayla Lane <lanesha@oregonstate.edu>
2021-01-18 10:34:59 -0800

    Abstract broadening or narrowing functionality from flexible search controller.

--- app/models/flexible_search.rb
+++ app/models/flexible_search.rb
@@ -3,4 +3,20 @@ class FlexibleSearch < ApplicationRecord
     has_many :specifications, dependent: :destroy
     has_many :tables, through: :specifications
 
+    def broad_or_narrow(flexible_search)
+        #assign parameters in a for each
+        manual_idspp1 = params[:manual_idspp1]
+        manual_idspp2 = params[:manual_idspp2]
+        night = params[:night]
+        if flexible_search.narrow?
+            @sonobat_outputs = SonobatOutput.where(manual_idspp1: manual_idspp1)
+            @sonobat_outputs = @sonobat_outputs.where(manual_idspp2: manual_idspp2)
+            @sonobat_outputs = @sonobat_outputs.where(night: night)
+            return @sonobat_outputs
+        else
+            @sonobat_outputs = SonobatOutput.where(manual_idspp1: manual_idspp1).or(SonobatOutput.where(manual_idspp2: manual_idspp2)).or(SonobatOutput.where(night: night))
+            return @sonobat_outputs        
+        end
+    end
+
 end
--- app/models/sonobat_output.rb
+++ app/models/sonobat_output.rb
@@ -2,14 +2,5 @@
 
 class SonobatOutput < ApplicationRecord
   belongs_to :deployment
-
-  def self.search(search)
-    print('*******USING SEARCH FUNCTION**********')
-    if search
-      where(["manual_idspp1 LIKE ?", "%#{search}%"])
-    else
-      all
-    end
-  end
   
 end

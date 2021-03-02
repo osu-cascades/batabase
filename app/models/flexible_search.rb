b256504afa482abdafe3f40420b6a1d8b5291dad
Shayla Lane <lanesha@oregonstate.edu>
2021-03-02 15:58:08 -0800

    Add search fields hash to flexible search.

--- app/models/flexible_search.rb
+++ app/models/flexible_search.rb
@@ -1,34 +1,41 @@
 class FlexibleSearch < ApplicationRecord
+    serialize :fields_hash
+
     before_save do
-        self[:search_field] = "search_fields = { "
+        self[:search_field] = "{ "
         @deployment_start = false
         @so_start = false
         self.searchables.each do |s|
-    
-        if parent_table(s) == "deployment" and @deployment_start == false
-            puts self[:search_field]
-            self[:search_field] << parent_table(s)
-            self[:search_field] << " => ["
-            @deployment_start = true
+            if parent_table(s) == "deployment" and @deployment_start == false
+                self[:search_field] << parent_table(s)
+                self[:search_field] << " => ["
+                @deployment_start = true
+            end
+            if @deployment_start == true and @so_start == false
+                self[:search_field] << "\""
+                self[:search_field] << s
+                self[:search_field] << "\", "
+            end
+            if parent_table(s) == "sonobat_output" and @so_start == false
+                self[:search_field] = self[:search_field] [0...-2]
+                if @deployment_start == true
+                    self[:search_field] << " ], "
+                end
+                self[:search_field] << "sonobat_output => [ "
+                @so_start = true
+            end
+            if parent_table(s) == "sonobat_output" and @so_start == true
+                self[:search_field] << "\""
+                self[:search_field] << s
+                self[:search_field] << "\", "
+            end
         end
-        if @deployment_start == true and @so_start == false
-            self[:search_field] << "'"
-            self[:search_field] << s
-            self[:search_field] << "', "
-        end
-        if parent_table(s) == "sonobat_output" and @so_start == false
-            self[:search_field] = self[:search_field] [0...-2]
-            self[:search_field] << " ], sonobat_output => [ "
-            @so_start = true
-        end
-        if parent_table(s) == "sonobat_output" and @so_start == true
-            self[:search_field] << "'"
-            self[:search_field] << s
-            self[:search_field] << "', "
-        end
+        self[:search_field] = self[:search_field][0...-2]
+        self[:search_field] << " ]}"
     end
-    self[:search_field] = self[:search_field][0...-2]
-    self[:search_field] << " ]}"
+
+    before_save do
+        self[:fields_hash] = eval(self[:search_field])
     end
 
     def parent_table(strng)
@@ -39,24 +46,21 @@ class FlexibleSearch < ApplicationRecord
         end
     end
 
-    def search_field
 
-    end
-
-    def search()
-        #assign parameters in a for each
-        manual_idspp1 = params[:manual_idspp1]
-        manual_idspp2 = params[:manual_idspp2]
-        night = params[:night]
-        if flexible_search.narrow?
-            @sonobat_outputs = SonobatOutput.where(manual_idspp1: manual_idspp1)
-            @sonobat_outputs = @sonobat_outputs.where(manual_idspp2: manual_idspp2)
-            @sonobat_outputs = @sonobat_outputs.where(night: night)
-            return @sonobat_outputs
-        else
-            @sonobat_outputs = SonobatOutput.where(manual_idspp1: manual_idspp1).or(SonobatOutput.where(manual_idspp2: manual_idspp2)).or(SonobatOutput.where(night: night))
-            return @sonobat_outputs        
-        end
-    end
+    # def search()
+    #     #assign parameters in a for each
+    #     manual_idspp1 = params[:manual_idspp1]
+    #     manual_idspp2 = params[:manual_idspp2]
+    #     night = params[:night]
+    #     if flexible_search.narrow?
+    #         @sonobat_outputs = SonobatOutput.where(manual_idspp1: manual_idspp1)
+    #         @sonobat_outputs = @sonobat_outputs.where(manual_idspp2: manual_idspp2)
+    #         @sonobat_outputs = @sonobat_outputs.where(night: night)
+    #         return @sonobat_outputs
+    #     else
+    #         @sonobat_outputs = SonobatOutput.where(manual_idspp1: manual_idspp1).or(SonobatOutput.where(manual_idspp2: manual_idspp2)).or(SonobatOutput.where(night: night))
+    #         return @sonobat_outputs        
+    #     end
+    # end
 
 end

a5ae96fbc5d1378d22704401264a3495d9831f5d
Yong Bakos <ybakos@humanoriented.com>
2021-04-23 15:18:05 -0700

    RansackQueriesController: Fix indentation.
    Always use two-space indents.
    cc: @kimberr @slane2

--- app/controllers/ransack_queries_controller.rb
+++ app/controllers/ransack_queries_controller.rb
@@ -1,46 +1,46 @@
 class RansackQueriesController < ApplicationController
 
-    def index
-        @ransack_queries = RansackQuery.all
+  def index
+    @ransack_queries = RansackQuery.all
+  end
+
+  def new
+    @ransack_query = RansackQuery.new
+  end
+
+  def show
+    @ransack_query = RansackQuery.find(params[:id])
+  end
+
+  def create
+    @ransack_query = RansackQuery.new(ransack_query_params)
+    if @ransack_query.save
+      redirect_to @ransack_query
+    else
+      render :new
     end
+  end
 
-    def new
-        @ransack_query = RansackQuery.new
+  def edit
+    @ransack_query = RansackQuery.find(params[:id])
+  end
+
+  def update
+    @ransack_query = RansackQuery.find(params[:id])
+    if @ransack_query.update(ransack_query_params)
+      redirect_to @ransack_query
+    else
+      render :edit
     end
+  end
 
-    def show
-        @ransack_query = RansackQuery.find(params[:id])
-    end
+  def destroy
+  end
 
-    def create
-        @ransack_query = RansackQuery.new(ransack_query_params)
+  private
 
-        if @ransack_query.save
-            redirect_to @ransack_query
-        else
-            render :new
-        end
-    end
+  def ransack_query_params
+    params.require(:ransack_query).permit(:name, :q)
+  end
 
-    def edit
-        @ransack_query = RansackQuery.find(params[:id])
-    end
-
-    def update
-        @ransack_query = RansackQuery.find(params[:id])
-        if @ransack_query.update(ransack_query_params)
-            redirect_to @ransack_query
-        else
-            render :edit
-        end
-    end
-
-    def destroy
-    end
-
-    private
-
-    def ransack_query_params
-        params.require(:ransack_query).permit(:name, :q)
-    end
 end

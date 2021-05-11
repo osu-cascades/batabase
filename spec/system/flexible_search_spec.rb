4b35795a400cea719c385a9534e260f8758d0897
Kimber Rabago <rabagok@oregonstate.edu>
2021-05-11 12:41:25 -0700

    Passing test to view a flexible searches results but needs FIXING

--- spec/system/flexible_search_spec.rb
+++ spec/system/flexible_search_spec.rb
@@ -16,12 +16,13 @@ RSpec.describe FlexibleSearch, type: :system do
       expect(page).to have_content('Quick Search')
     end
 
-    # it 'A user can visit the page to add new flexible search' do
-    #   visit home_index_path
-    #   click_on 'Custom Search'
-    #   click_on 'Search #1'
+    it 'A user can visit the page and view a flexible search' do
+      visit home_index_path
+      click_on 'Custom Search'
+      visit quick_search_1_path
+      # click_on 'Search #1'
 
-    #   expect(page).to have_content('Search Results')
-    # end
+      expect(page).to have_content('Search Results')
+    end
   end
 end
